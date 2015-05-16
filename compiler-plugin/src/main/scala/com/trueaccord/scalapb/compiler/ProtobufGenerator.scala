4fc52e3d798692dfeb69e9ecc816d56ad455277a
Nadav Sr. Samet <thesamet@gmail.com>
2015-05-15 20:17:20 -0700

    Bugfix: allow message fields to be named as scala reserved words.

--- compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/ProtobufGenerator.scala
+++ compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/ProtobufGenerator.scala
@@ -445,7 +445,7 @@ class ProtobufGenerator(val params: GeneratorParams) extends DescriptorPimps {
           val newValBase = if (field.getJavaType == JavaType.MESSAGE) {
             val defInstance = s"${field.getMessageType.scalaTypeName}.defaultInstance"
             val baseInstance = if (field.isOptional && !field.isInOneof) {
-              val expr = s"__${fieldAccessorSymbol(field)}"
+              val expr = s"__${field.scalaName}"
               s"${mapToBaseType(field)(expr)}.getOrElse($defInstance)"
             } else if (field.isInOneof) {
               s"${mapToBaseType(field)(fieldAccessorSymbol(field))}.getOrElse($defInstance)"
