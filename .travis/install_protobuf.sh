51fc7751d52d59fc5175a7dacdf76d9cd9eb2ec8
Nadav Sr. Samet <thesamet@gmail.com>
2015-06-16 22:28:31 -0700

    Remove travis protocol buffer build step.

--- .travis/install_protobuf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-PROTOBUF_VERSION=3.0.0-alpha-2
-wget https://github.com/google/protobuf/archive/v${PROTOBUF_VERSION}.tar.gz
-tar -xzvf v${PROTOBUF_VERSION}.tar.gz
-cd protobuf-${PROTOBUF_VERSION} && ./autogen.sh && ./configure --prefix=/usr && make && sudo make install
-
