e9d8d5db20d6ab796f86ceb0ebdf1d6791c66e19
Nadav Sr. Samet <thesamet@gmail.com>
2015-08-22 17:14:58 -0700

    Set spbc to protoc v2.6.1

--- spbc/src/main/scala/com/trueaccord/scalapb/Spbc.scala
+++ spbc/src/main/scala/com/trueaccord/scalapb/Spbc.scala
@@ -5,7 +5,7 @@ import com.trueaccord.scalapb.compiler.ProtocDriverFactory
 object Spbc extends App {
   val code = ProtocDriverFactory.create().buildRunner({
     args =>
-      com.github.os72.protocjar.Protoc.runProtoc("-v300" +: args.toArray)
+      com.github.os72.protocjar.Protoc.runProtoc("-v261" +: args.toArray)
   })(args)
   sys.exit(code)
 }
