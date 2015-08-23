7bee279ae86f6b1251a1ab36083311dfcfc0595b
Nadav Sr. Samet <thesamet@gmail.com>
2015-08-22 22:49:48 -0700

    Rename spbc to scalapbc

--- scalapbc/src/main/scala/com/trueaccord/scalapb/ScalaPBC.scala
+++ scalapbc/src/main/scala/com/trueaccord/scalapb/ScalaPBC.scala
@@ -2,7 +2,7 @@ package com.trueacord.scalapb
 
 import com.trueaccord.scalapb.compiler.ProtocDriverFactory
 
-object Spbc extends App {
+object ScalaPBC extends App {
   val code = ProtocDriverFactory.create().buildRunner({
     args =>
       com.github.os72.protocjar.Protoc.runProtoc("-v300" +: args.toArray)
