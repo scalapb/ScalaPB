b009cd16465c350e56af6f17555d61ad6cfbdadc
Nadav Samet <thesamet@gmail.com>
2015-11-14 22:38:39 -0800

    Initial e2e spec for  field.

---
+++ e2e/src/test/scala/BytesSpec.scala
@@ -0,0 +1,18 @@
+import com.google.protobuf.ByteString
+import com.trueaccord.proto.e2e.bytes._
+import org.scalatest._
+
+class BytesSpec extends FlatSpec with MustMatchers {
+
+  "fromFieldsMap" should "work when empty" in {
+    val b = ByteMessage()
+    ByteMessage.fromFieldsMap(b.getAllFields) must be(b)
+  }
+
+  "fromFieldsMap" should "work when non-empty" in {
+    val b = ByteMessage(s = ByteString.copyFrom(Array[Byte](17, 54)))
+    ByteMessage.fromFieldsMap(b.getAllFields) must be(b)
+  }
+
+}
+
