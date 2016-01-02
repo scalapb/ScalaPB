1a2009c5691e090ad67eb30023a15afae6e46f21
Nadav Samet <thesamet@gmail.com>
2016-01-02 00:31:26 -0800

    Fix e2e.sh script

--- e2e.sh
+++ e2e.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
-sbt ++2.10.6 compilerPlugin/publishLocal createVersionFile \
+sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
     ++2.11.7 runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
 sbt clean test
