75fca753125da926b791ced2053407e436faaa46
Andreas Koestler <andreas.koestler@gmail.com>
2016-11-30 19:10:58 +0100

    Implement all interface types

--- compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/GrpcServiceIFacePrinter.scala
+++ compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/GrpcServiceIFacePrinter.scala
@@ -5,35 +5,82 @@ import com.trueaccord.scalapb.compiler.FunctionalPrinter.PrinterEndo
 
 final class GrpcServiceIFacePrinter(service: ServiceDescriptor, override val params: GeneratorParams) extends DescriptorPimps {
   
-  private[this] def service(reqTypeParam: String, repTypeParam: String): String = s"Service[$reqTypeParam, $repTypeParam]"
+  private[this] def unaryService(reqTypeParam: String, repTypeParam: String): String = s"Service[$reqTypeParam, $repTypeParam]"
 
-  private[this] val serviceTypeAlias: String = s"type Service[Req, Rep] = Req => scala.concurrent.Future[Rep]"
+  private[this] def clientStreamingService(reqTypeParam: String, repTypeParam: String): String = s"ClientStreamingService[$repTypeParam, $reqTypeParam]"
 
-  private[this] def serviceParamSignature(method: MethodDescriptor) =
-    s"${method.name}: ${service(method.scalaIn, method.scalaOut)}"
+  private[this] def serverStreamingService(reqTypeParam: String, reqTypeParam2: String): String = s"ServerStreamingService[$reqTypeParam, $reqTypeParam2]"
 
+  private[this] def bidirectionalService(reqTypeParam: String, repTypeParam: String): String = s"BidirectionalService[$repTypeParam, $reqTypeParam]"
+
+  private[this] val unaryServiceTypeAlias: String = s"type Service[Req, Rep] = Req => scala.concurrent.Future[Rep]"
+
+  private[this] val clientStreamingServiceTypeAlias: String = "type ClientStreamingService[Req, Rep] = _root_.io.grpc.stub.StreamObserver[Req] => _root_.io.grpc.stub.StreamObserver[Rep]"
+
+  private[this] val serverStreamingServiceTypeAlias: String = "type ServerStreamingService[Req, Req2] = (Req, _root_.io.grpc.stub.StreamObserver[Req2]) => Unit"
+
+  private[this] val bidirectionalServiceTypeAlias: String = "type BidirectionalService[Req, Rep] = _root_.io.grpc.stub.StreamObserver[Req] => _root_.io.grpc.stub.StreamObserver[Rep]"
+
+  private[this] def functionTypeSig(from: String, to: String): String = s"$from => $to"
+
+  private[this] def observer(typeParam: String): String = s"$streamObserver[$typeParam]"
+
+  private[this] val streamObserver = "_root_.io.grpc.stub.StreamObserver"
+
+
+  private[this] def serviceParam(method: MethodDescriptor) = s"${method.name}: ${serviceTypeSignature(method)}"
+
+  private[this] def serviceTypeSignature(method: MethodDescriptor) = 
+    (method.streamType match {
+      case StreamType.Unary =>
+        s"${unaryService(method.scalaIn, method.scalaOut)}"
+      case StreamType.ClientStreaming =>
+        s"${clientStreamingService(method.scalaIn, method.scalaOut)}"
+      case StreamType.ServerStreaming =>
+        s"${serverStreamingService(method.scalaIn, method.scalaOut)}"
+      case StreamType.Bidirectional =>
+        s"${bidirectionalService(method.scalaIn, method.scalaOut)}"
+    })
   private[this] def methodAlias(method: MethodDescriptor) = s"${method.name}Alias"
-
+  
   private[this] def serviceAliasDefinition(method: MethodDescriptor) =
-    s"val ${methodAlias(method)}: ${service(method.scalaIn, method.scalaOut)} = ${method.name}"
+    s"val ${methodAlias(method)}: ${serviceTypeSignature(method)} = ${method.name}"
 
   private[this] def serviceMethodSignature(method: MethodDescriptor) = 
-    s"def ${method.name}(request: ${method.scalaIn}): scala.concurrent.Future[${method.scalaOut}]"
+    s"def ${method.name}" + (method.streamType match {
+      case StreamType.Unary =>
+        s"(request: ${method.scalaIn}): scala.concurrent.Future[${method.scalaOut}]"
+      case StreamType.ClientStreaming =>
+        s"(responseObserver: ${observer(method.scalaOut)}): ${observer(method.scalaIn)}"
+      case StreamType.ServerStreaming =>
+        s"(request: ${method.scalaIn}, responseObserver: ${observer(method.scalaOut)}): Unit"
+      case StreamType.Bidirectional =>
+        s"(responseObserver: ${observer(method.scalaOut)}): ${observer(method.scalaIn)}"
+    })
 
-  private[this] def serviceInterfaceMethodsDefinition(method: MethodDescriptor) =
-    s"${serviceMethodSignature(method)} = ${methodAlias(method)}(request)"
+  private[this] def serviceInterfaceMethodDefinition(method: MethodDescriptor) = 
+    s"${serviceMethodSignature(method)} = " + (method.streamType match {
+      case StreamType.Unary =>
+        s"${methodAlias(method)}(request)"
+      case StreamType.ClientStreaming =>
+         s"${methodAlias(method)}(responseObserver)"
+      case StreamType.ServerStreaming =>
+         s"${methodAlias(method)}(request, responseObserver)"
+      case StreamType.Bidirectional =>
+         s"${methodAlias(method)}(responseObserver)"
+    })
 
   private[this] def serviceImport = s"import ${service.objectName}._"
 
   private[this] def serviceIFaceConstructor: PrinterEndo = {
-    val params: String = service.methods.map(m => serviceParamSignature(m)).mkString(",\n")
+    val params: String = service.methods.map(m => serviceParam(m)).mkString(",\n")
 
     val aliases: PrinterEndo = { p =>
       p.seq(service.methods.map(m => serviceAliasDefinition(m)))
     }
 
     val implementations: PrinterEndo = { p =>
-      p.seq(service.methods.map(m => serviceInterfaceMethodsDefinition(m)))
+      p.seq(service.methods.map(m => serviceInterfaceMethodDefinition(m)))
     }
     p =>
     p
@@ -60,10 +107,14 @@ final class GrpcServiceIFacePrinter(service: ServiceDescriptor, override val par
       .newline
       .add(s"object ${service.objectName}ServiceI {")
       .indent
-      .add(serviceTypeAlias)
+      .add(unaryServiceTypeAlias)
+      .add(clientStreamingServiceTypeAlias)
+      .add(serverStreamingServiceTypeAlias)
+      .add(bidirectionalServiceTypeAlias)
       .newline
       .call(serviceIFaceConstructor)
       .outdent
       .add("}")
   }
+
 }
