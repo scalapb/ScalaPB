9a395b52e864f250bc984f7b70511c7989ab1cd0
Nadav Samet <thesamet@gmail.com>
2017-03-18 09:31:38 -0700

    Move test that generated code is checked in to auxiliary script.

---
+++ test_generated_code_checked_in.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Test that all generated code is checked in.
+set -e
+
+echo "java -jar ${HOME}/.ivy2/cache/com.github.os72/protoc-jar/jars/protoc-jar-3.2.0.jar \$@" > protoc
+chmod +x protoc
+PATH=${PATH}:${PWD} ./make_plugin_proto.sh
+rm protoc
+git diff --exit-code || { \
+    echo "Generated code changed. Please run make_plugin_proto.sh."; exit 1; }
+
