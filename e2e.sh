a285b4e8d8e55f6779578f2aa7ef026051029dac
Nadav Samet <thesamet@gmail.com>
2017-03-24 22:54:20 -0700

    Make e2e.sh compile using a Scala version provided by an env variable.
    Prior to this change, e2e.sh always compiled for 2.11.7, now e2e.sh
    takes the value from the environment variable $SCALA_VERSION, and falls
    back to $TRAVIS_SCALA_VERSION. This ensures that we run e2e.sh on travis
    for all versions that we ship.
    
    Fixes #259

--- e2e.sh
+++ e2e.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -e
+SCALA_VERSION=${SCALA_VERSION:-${TRAVIS_SCALA_VERSION:-2.11.7}}
+
 sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
-    ++2.11.8 runtimeJVM/publishLocal grpcRuntime/publishLocal
+    ++$SCALA_VERSION runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
-sbt clean test
+sbt ++$SCALA_VERSION clean test
 
