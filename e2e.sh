564077af56fd743ed930853669d6fe26b38189d5
Nadav Samet <thesamet@gmail.com>
2017-03-24 22:59:04 -0700

    e2e: do not publish local 2.10.x runtimeJVM

--- e2e.sh
+++ e2e.sh
@@ -2,7 +2,7 @@
 set -e
 SCALA_VERSION=${SCALA_VERSION:-${TRAVIS_SCALA_VERSION:-2.11.8}}
 
-sbt ++2.10.6 compilerPlugin/publishLocal runtimeJVM/publishLocal createVersionFile \
+sbt ++2.10.6 compilerPlugin/publishLocal createVersionFile \
     ++$SCALA_VERSION runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
 sbt ++$SCALA_VERSION clean test
