b3457d1f5848358dff971578621437563909b11f
Nadav Samet <thesamet@gmail.com>
2017-06-10 21:49:36 -0700

    Add e2e tests for shaded compiler plugin

--- e2e.sh
+++ e2e.sh
@@ -2,7 +2,13 @@
 set -e
 SCALA_VERSION=${SCALA_VERSION:-${TRAVIS_SCALA_VERSION:-2.11.11}}
 
-sbt ++2.10.6 compilerPlugin/publishLocal createVersionFile \
+# E2E_SHADED signals we will work with the shaded version of the
+# compilerplugin.
+if [ $E2E_SHADED = "1" ]; then
+  mv e2e/project/scalapb.sbt.shaded e2e/project/scalapb.sbt
+fi
+
+sbt ++2.10.6 compilerPlugin/publishLocal compilerPluginShaded/publishLocal createVersionFile \
     ++$SCALA_VERSION runtimeJVM/publishLocal grpcRuntime/publishLocal
 cd e2e
 sbt ++$SCALA_VERSION noJava/clean clean noJava/test test
