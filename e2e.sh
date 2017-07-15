1d3decd6ab6a5250b6563c9e3e815ba6e237ab5c
Nadav Samet <thesamet@gmail.com>
2017-07-15 12:24:48 -0700

    Make e2e_shaded check work when var is not defined.

--- e2e.sh
+++ e2e.sh
@@ -4,7 +4,7 @@ SCALA_VERSION=${SCALA_VERSION:-${TRAVIS_SCALA_VERSION:-2.11.11}}
 
 # E2E_SHADED signals we will work with the shaded version of the
 # compilerplugin.
-if [ $E2E_SHADED = "1" ]; then
+if [ "$E2E_SHADED" = "1" ]; then
   mv e2e/project/scalapb.sbt.shaded e2e/project/scalapb.sbt
 fi
 
