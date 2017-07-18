f0bf07d8fb9e88f418eefc3022f9f9ac4e10f1bc
David Pratt <david.pratt@gmail.com>
2017-07-18 11:12:02 -0500

    Update async observer completion.
    - Handle StatusRuntimeExceptions as well as StatusExceptions on Observer
    completion.
    - Use tryFailure and trySuccess for Guava -> Scala Future translation.
    This avoids a spurious exception thrown on (presumedly very rare)
    multiple completion of the Future.

--- scalapb-runtime-grpc/src/main/scala/com/trueaccord/scalapb/grpc/Grpc.scala
+++ scalapb-runtime-grpc/src/main/scala/com/trueaccord/scalapb/grpc/Grpc.scala
@@ -1,9 +1,8 @@
 package com.trueaccord.scalapb.grpc
 
 import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
-import io.grpc.{Status, StatusException}
+import io.grpc.{Status, StatusException, StatusRuntimeException}
 import io.grpc.stub.StreamObserver
-
 import scala.concurrent.{Future, Promise}
 import scala.util.Try
 
@@ -11,8 +10,8 @@ object Grpc {
   def guavaFuture2ScalaFuture[A](guavaFuture: ListenableFuture[A]): Future[A] = {
     val p = Promise[A]()
     Futures.addCallback(guavaFuture, new FutureCallback[A] {
-      override def onFailure(t: Throwable) = p.failure(t)
-      override def onSuccess(a: A) = p.success(a)
+      override def onFailure(t: Throwable) = p.tryFailure(t)
+      override def onSuccess(a: A) = p.trySuccess(a)
     })
     p.future
   }
@@ -22,6 +21,8 @@ object Grpc {
       observer.onNext(value)
       observer.onCompleted()
     case scala.util.Failure(s: StatusException) =>
+      observer.onError(s)
+    case scala.util.Failure(s: StatusRuntimeException) =>
       observer.onError(s)
     case scala.util.Failure(e) =>
       observer.onError(
