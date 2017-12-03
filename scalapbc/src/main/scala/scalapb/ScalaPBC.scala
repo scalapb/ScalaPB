de348ebb56abeca256bf2db55102f89a3ff3483c
Nadav Samet <thesamet@gmail.com>
2017-12-03 12:27:07 -0800

    Scalapbc: Add support for dynamically loading generators.

--- scalapbc/src/main/scala/scalapb/ScalaPBC.scala
+++ scalapbc/src/main/scala/scalapb/ScalaPBC.scala
@@ -2,18 +2,21 @@ package scalapb
 
 import java.io.File
 
-import protocbridge.ProtocBridge
+import protocbridge.{ProtocBridge, ProtocCodeGenerator}
 
 case class Config(
   version: String = "-v310",
   throwException: Boolean = false,
   args: Seq[String] = Seq.empty,
-  customProtocLocation: Option[String] = None)
+  customProtocLocation: Option[String] = None,
+  namedGenerators: Seq[(String, ProtocCodeGenerator)] = Seq("scala" -> ScalaPbCodeGenerator)
+)
 
 class ScalaPbcException(msg: String) extends RuntimeException(msg)
 
 object ScalaPBC {
-  private val customPathArgument =  "--protoc="
+  private val CustomPathArgument =  "--protoc="
+  private val CustomGenArgument = "--custom-gen="
 
   def processArgs(args: Array[String]): Config = {
     case class State(cfg: Config, passThrough: Boolean)
@@ -23,7 +26,13 @@ object ScalaPBC {
         (state.passThrough, item) match {
           case (false, v) if v.startsWith("-v") => state.copy(cfg = state.cfg.copy(version = v))
           case (false, "--throw") => state.copy(cfg = state.cfg.copy(throwException = true))
-          case (false, p) if p.startsWith(customPathArgument) => state.copy(cfg = state.cfg.copy(customProtocLocation = Some(p.substring(customPathArgument.length))))
+          case (false, p) if p.startsWith(CustomGenArgument) =>
+            val Array(genName, klassName) = p.substring(CustomGenArgument.length).split('=')
+            val klass = Class.forName(klassName + "$")
+            val gen = klass.getField("MODULE$").get(klass).asInstanceOf[ProtocCodeGenerator]
+            state.copy(cfg = state.cfg.copy(namedGenerators = state.cfg.namedGenerators :+ (genName -> gen)))
+          case (false, p) if p.startsWith(CustomPathArgument) =>
+            state.copy(cfg = state.cfg.copy(customProtocLocation = Some(p.substring(CustomPathArgument.length))))
           case (_, other) => state.copy(
             passThrough = true, cfg=state.cfg.copy(args = state.cfg.args :+ other))
         }
@@ -41,7 +50,7 @@ object ScalaPBC {
         case None =>
           a => com.github.os72.protocjar.Protoc.runProtoc(config.version +: a.toArray)
       },
-      namedGenerators = Seq("scala" -> ScalaPbCodeGenerator),
+      namedGenerators = config.namedGenerators,
       params = config.args)
 
     if (!config.throwException) {
