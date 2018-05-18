9950b04e3b051dda98e166c1ae6a09b1bc0b6285
Nadav Samet <thesamet@gmail.com>
2018-05-18 08:08:48 +0200

    Update ScalaPBC to use protoc 3.5.1 by default

--- scalapbc/src/main/scala/scalapb/ScalaPBC.scala
+++ scalapbc/src/main/scala/scalapb/ScalaPBC.scala
@@ -5,7 +5,7 @@ import java.io.File
 import protocbridge.{ProtocBridge, ProtocCodeGenerator}
 
 case class Config(
-  version: String = "-v310",
+  version: String = "-v351",
   throwException: Boolean = false,
   args: Seq[String] = Seq.empty,
   customProtocLocation: Option[String] = None,
