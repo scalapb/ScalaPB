870aaf38a4525bbbaf4d57b39252d63d72d9c009
Nadav Samet <thesamet@gmail.com>
2018-05-28 19:04:25 -0700

    Update lenses compatibility aliases

--- lenses/shared/src/main/scala/com/trueaccord/lenses/package.scala
+++ lenses/shared/src/main/scala/com/trueaccord/lenses/package.scala
@@ -2,14 +2,14 @@ package com.trueaccord
 
 package object lenses {
   @deprecated("Use scalapb.lenses package instead of com.trueaccord.lenses", "0.7.0")
-  type Updatable[A] = scalapb.lenses.Updatable[A]
+  type Updatable[A] = _root_.scalapb.lenses.Updatable[A]
 
   @deprecated("Use scalapb.lenses package instead of com.trueaccord.lenses", "0.7.0")
-  type Lens[Container, A] = scalapb.lenses.Lens[Container, A]
+  type Lens[Container, A] = _root_.scalapb.lenses.Lens[Container, A]
 
   @deprecated("Use scalapb.lenses package instead of com.trueaccord.lenses", "0.7.0")
-  type ObjectLens[U, Container] = scalapb.lenses.ObjectLens[U, Container]
+  type ObjectLens[U, Container] = _root_.scalapb.lenses.ObjectLens[U, Container]
 
   @deprecated("Use scalapb.lenses package instead of com.trueaccord.lenses", "0.7.0")
-  val Lens = scalapb.lenses.Lens
+  val Lens = _root_.scalapb.lenses.Lens
 }
