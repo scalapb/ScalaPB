f17562afffc791105c70611b1961365599aa21db
Nadav Samet <thesamet@gmail.com>
2018-09-29 22:25:54 -0700

    Download protoc-jar if it is not available

--- test_generated_code_checked_in.sh
+++ test_generated_code_checked_in.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 # Test that all generated code is checked in.
 set -e
+PROTOC_JAR_VERSION=3.6.0
+JAR_FILENAME=protoc-jar-$PROTOC_JAR_VERSION.jar
+JAR_PATH=${HOME}/.ivy2/cache/com.github.os72/protoc-jar/jars/$JAR_FILENAME
+JAR_URL=http://central.maven.org/maven2/com/github/os72/protoc-jar/$PROTOC_JAR_VERSION/$JAR_FILENAME
 
-echo "java -jar ${HOME}/.ivy2/cache/com.github.os72/protoc-jar/jars/protoc-jar-3.6.0.jar -v351 \$@" > protoc
+if [ ! -f $JAR_PATH ]; then
+    curl $JAR_URL -o $JAR_PATH;
+fi
+
+echo "java -jar $JAR_PATH -v360 \$@" > protoc
 chmod +x protoc
 PATH=${PATH}:${PWD} ./make_plugin_proto.sh
 rm protoc
