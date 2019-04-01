744363858a52751f45c95ecdbc6506c036615ce6
Nadav Samet <thesamet@gmail.com>
2019-03-31 20:19:45 -0700

    Make proptest generate SBT projects

--- proptest.sh
+++ proptest.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 set -e
-sbt ++$TRAVIS_SCALA_VERSION test
+sbt ++2.12.8 compilerPlugin/publishLocal \
+    ++$TRAVIS_SCALA_VERSION \
+    lensesJVM/publishLocal runtimeJVM/publishLocal grpcRuntime/publishLocal test
