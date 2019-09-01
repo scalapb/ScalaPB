a5b95f8978c06eae2e23814e6f85f38569a5bfbe
Koji AGAWA <atty303@gmail.com>
2019-09-01 17:01:05 +0900

    pass only methodDescriptor to ConcreteProtoFileDescriptorSupplier

--- scalapb-runtime-grpc/src/main/scala/scalapb/grpc/ConcreteProtoMethodDescriptorSupplier.scala
+++ scalapb-runtime-grpc/src/main/scala/scalapb/grpc/ConcreteProtoMethodDescriptorSupplier.scala
@@ -4,12 +4,10 @@ import com.google.protobuf.Descriptors
 import io.grpc.protobuf.{ProtoMethodDescriptorSupplier, ProtoServiceDescriptorSupplier}
 
 class ConcreteProtoMethodDescriptorSupplier(
-    fileDescriptor: Descriptors.FileDescriptor,
-    serviceDescriptor: Descriptors.ServiceDescriptor,
     methodDescriptor: Descriptors.MethodDescriptor
 ) extends ProtoMethodDescriptorSupplier
     with ProtoServiceDescriptorSupplier {
   override def getMethodDescriptor: Descriptors.MethodDescriptor   = methodDescriptor
-  override def getServiceDescriptor: Descriptors.ServiceDescriptor = serviceDescriptor
-  override def getFileDescriptor: Descriptors.FileDescriptor       = fileDescriptor
+  override def getServiceDescriptor: Descriptors.ServiceDescriptor = methodDescriptor.getService
+  override def getFileDescriptor: Descriptors.FileDescriptor       = getServiceDescriptor.getFile
 }
