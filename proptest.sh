b6ccaaca824556a87990d0ba5b23154eae19645e
Ondra Pelech <ondra.pelech@gmail.com>
2019-09-17 20:14:11 +0200

    proptest.sh give back memory to OS

--- proptest.sh
+++ proptest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 set -e
-sbt -J-XX:LoopStripMiningIter=0 -J-Xmx4500M ++$TRAVIS_SCALA_VERSION test
+sbt -J-XX:LoopStripMiningIter=0 -J-Xmx4500M -J-XX:GCTimeRatio=19 -J-XX:MinHeapFreeRatio=10 -J-XX:MaxHeapFreeRatio=20 ++$TRAVIS_SCALA_VERSION test
