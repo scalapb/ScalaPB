6d8814a1a8264cbcf358374ad6d612653cc72dd9
Ondra Pelech <ondra.pelech@gmail.com>
2019-10-17 16:45:12 +0200

    fix EXAMPLES_AND_FORMATTING test

--- scalapb-runtime/shared/src/main/scala/scalapb/options/ScalaPbOptions.scala
+++ scalapb-runtime/shared/src/main/scala/scalapb/options/ScalaPbOptions.scala
@@ -541,6 +541,7 @@ object ScalaPbOptions extends scalapb.GeneratedMessageCompanion[scalapb.options.
     def isAsInProto: _root_.scala.Boolean = false
     def isCamelCase: _root_.scala.Boolean = false
     def companion: _root_.scalapb.GeneratedEnumCompanion[EnumValueNaming] = scalapb.options.ScalaPbOptions.EnumValueNaming
+    final def asRecognized: _root_.scala.Option[scalapb.options.ScalaPbOptions.EnumValueNaming.Recognized] = if (isUnrecognized) _root_.scala.None else _root_.scala.Some(this.asInstanceOf[scalapb.options.ScalaPbOptions.EnumValueNaming.Recognized])
   }
   
   object EnumValueNaming extends _root_.scalapb.GeneratedEnumCompanion[EnumValueNaming] {
@@ -548,7 +549,7 @@ object ScalaPbOptions extends scalapb.GeneratedMessageCompanion[scalapb.options.
     /** Enum value names in Scala use the same name as in the proto
       */
     @SerialVersionUID(0L)
-    case object AS_IN_PROTO extends EnumValueNaming {
+    case object AS_IN_PROTO extends EnumValueNaming with EnumValueNaming.Recognized {
       val value = 0
       val index = 0
       val name = "AS_IN_PROTO"
@@ -558,7 +559,7 @@ object ScalaPbOptions extends scalapb.GeneratedMessageCompanion[scalapb.options.
     /** Convert enum values to CamelCase in Scala.
       */
     @SerialVersionUID(0L)
-    case object CAMEL_CASE extends EnumValueNaming {
+    case object CAMEL_CASE extends EnumValueNaming with EnumValueNaming.Recognized {
       val value = 1
       val index = 1
       val name = "CAMEL_CASE"
@@ -574,6 +575,7 @@ object ScalaPbOptions extends scalapb.GeneratedMessageCompanion[scalapb.options.
       case 1 => CAMEL_CASE
       case __other => Unrecognized(__other)
     }
+    sealed trait Recognized extends EnumValueNaming
     def javaDescriptor: _root_.com.google.protobuf.Descriptors.EnumDescriptor = scalapb.options.ScalaPbOptions.javaDescriptor.getEnumTypes.get(1)
     def scalaDescriptor: _root_.scalapb.descriptors.EnumDescriptor = scalapb.options.ScalaPbOptions.scalaDescriptor.enums(1)
   }
