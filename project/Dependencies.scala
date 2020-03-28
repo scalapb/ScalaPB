126aab8903a29d9773b87a9d402adadd536955f2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-28 06:42:33 +0100

    Update protobuf-runtime-scala to 0.8.5 (#794)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val mockito                 = "3.2.0"
     val protocBridge            = "0.8.1"
     val protocJar               = "3.11.4"
-    val protobufRuntimeScala    = "0.8.4"
+    val protobufRuntimeScala    = "0.8.5"
     val commonsCodec            = "1.14"
     val scalaTest               = "3.1.1"
     val scalaTestPlusScalaCheck = "3.1.1.1"
