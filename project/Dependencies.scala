fd04f012d6072be781fe358d3c28c4cef21afcc9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-05-10 17:35:00 +0200

    Update scalatest to 3.1.2 (#824)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val protocJar               = "3.11.4"
     val protobufRuntimeScala    = "0.8.5"
     val commonsCodec            = "1.14"
-    val scalaTest               = "3.1.1"
+    val scalaTest               = "3.1.2"
     val scalaTestPlusScalaCheck = "3.1.1.1"
     val scalaTestPlusMockito    = "3.1.0.0"
     val annotationApi           = "1.3.2"
