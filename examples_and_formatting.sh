7c00b34c37d8ec48604ea146e4645f57e64f39b7
Nadav Samet <thesamet@gmail.com>
2020-06-13 22:36:53 -0700

    Examples and formatting

---
+++ examples_and_formatting.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e 
+./test_generated_code_checked_in.sh
+for d in examples/*; do cd "$d" && sbt test && cd ../..; done
+sbt -J-Xmx4500M scalafmtCheck test:scalafmtCheck scalafmtSbtCheck
