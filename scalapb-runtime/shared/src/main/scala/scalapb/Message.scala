9d1b13e1244780704348e93dce53241ae6ac6ec3
Nadav Samet <thesamet@gmail.com>
2020-06-14 14:46:13 -0700

    Reintroduce scalapb package object. Will be removed at 0.11.x

--- scalapb-runtime/shared/src/main/scala/scalapb/Message.scala
+++
@@ -1,4 +0,0 @@
-package scalapb
-
-@deprecated("This trait is no longer used and can be safely removed.", since = "0.10.0")
-trait Message[A] extends Any
---
+++ scalapb-runtime/shared/src/main/scala/scalapb/package.scala
@@ -0,0 +1,4 @@
+package object scalapb {
+  @deprecated("This trait is no longer used and can be safely removed.", since = "0.10.0")
+  type Message[A] = Any
+}
