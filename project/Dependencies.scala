92b81c497fe1aa8b4a934ca33c024098d2d3c9fb
Scala Steward <me@scala-steward.org>
2020-07-07 20:53:34 +0200

    Update coursier to 2.0.0-RC6-22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val fastparse               = "2.3.0"
     val silencer                = "1.6.0"
     val collectionCompat        = "2.1.6"
-    val coursier                = "2.0.0-RC6-21"
+    val coursier                = "2.0.0-RC6-22"
     val mockito                 = "3.2.0"
     val protocGen               = "0.9.0-RC3"
     val protocJar               = "3.11.4"
