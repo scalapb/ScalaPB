ea44e012c95524f00041f120dc2a6d3f7d32ccd2
Nadav Samet <thesamet@gmail.com>
2020-07-30 20:16:14 -0700

    Fix deprecated implicits for Scala 2.13

--- compiler-plugin/src/main/scala/scalapb/compiler/DeprecatedImplicits.scala
+++ compiler-plugin/src/main/scala/scalapb/compiler/DeprecatedImplicits.scala
@@ -10,50 +10,57 @@ abstract class DeprecatedImplicits {
     "MethodDescriptorPimp class has been deprecated, use ExtendedMethodDescriptor instead.",
     "0.11.0"
   )
-  implicit final def MethodDescriptorPimp(self: MethodDescriptor) =
+  implicit final def MethodDescriptorPimp(self: MethodDescriptor): ExtendedMethodDescriptor =
     new ExtendedMethodDescriptor(self)
 
   @deprecated(
     "ServiceDescriptorPimp class has been deprecated, use ExtendedServiceDescriptor instead.",
     "0.11.0"
   )
-  implicit final def ServiceDescriptorPimp(self: ServiceDescriptor) =
+  implicit final def ServiceDescriptorPimp(self: ServiceDescriptor): ExtendedServiceDescriptor =
     new ExtendedServiceDescriptor(self)
 
   @deprecated(
     "FieldDescriptorPimp class has been deprecated, use ExtendedFieldDescriptor instead.",
     "0.11.0"
   )
-  implicit final def FieldDescriptorPimp(self: FieldDescriptor) = new ExtendedFieldDescriptor(self)
+  implicit final def FieldDescriptorPimp(self: FieldDescriptor): ExtendedFieldDescriptor =
+    new ExtendedFieldDescriptor(self)
 
   @deprecated(
     "MessageDescriptorPimp class has been deprecated, use ExtendedMessageDescriptor instead.",
     "0.11.0"
   )
-  implicit final def MessageDescriptorPimp(self: Descriptor) = new ExtendedMessageDescriptor(self)
+  implicit final def MessageDescriptorPimp(self: Descriptor): ExtendedMessageDescriptor =
+    new ExtendedMessageDescriptor(self)
 
   @deprecated(
     "OneofDescriptorPimp class has been deprecated, use ExtendedOneofDescriptor instead.",
     "0.11.0"
   )
-  implicit final def OneofDescriptorPimp(self: OneofDescriptor) = new ExtendedOneofDescriptor(self)
+  implicit final def OneofDescriptorPimp(self: OneofDescriptor): ExtendedOneofDescriptor =
+    new ExtendedOneofDescriptor(self)
 
   @deprecated(
     "EnumDescriptorPimp class has been deprecated, use ExtendedEnumDescriptor instead.",
     "0.11.0"
   )
-  implicit final def EnumDescriptorPimp(self: EnumDescriptor) = new ExtendedEnumDescriptor(self)
+  implicit final def EnumDescriptorPimp(self: EnumDescriptor): ExtendedEnumDescriptor =
+    new ExtendedEnumDescriptor(self)
 
   @deprecated(
     "EnumValueDescriptorPimp class has been deprecated, use ExtendedEnumValueDescriptor instead.",
     "0.11.0"
   )
-  implicit final def EnumValueDescriptorPimp(self: EnumValueDescriptor) =
+  implicit final def EnumValueDescriptorPimp(
+      self: EnumValueDescriptor
+  ): ExtendedEnumValueDescriptor =
     new ExtendedEnumValueDescriptor(self)
 
   @deprecated(
     "FileDescriptorPimp class has been deprecated, use ExtendedEnumValueDescriptor instead.",
     "0.11.0"
   )
-  implicit final def FileDescriptorPimp(self: FileDescriptor) = new ExtendedFileDescriptor(self)
+  implicit final def FileDescriptorPimp(self: FileDescriptor): ExtendedFileDescriptor =
+    new ExtendedFileDescriptor(self)
 }
