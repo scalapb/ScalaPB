21511a44453539881b8eb346b8e0a42374c93ba0
Scala Steward <me@scala-steward.org>
2020-08-23 18:41:31 +0200

    Update scalacheck-1-14 to 3.2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val protobufRuntimeScala    = "0.8.6"
     val commonsCodec            = "1.14"
     val scalaTest               = "3.2.2"
-    val scalaTestPlusScalaCheck = "3.2.1.0"
+    val scalaTestPlusScalaCheck = "3.2.2.0"
     val scalaTestPlusMockito    = "3.1.0.0"
     val annotationApi           = "1.3.2"
   }
