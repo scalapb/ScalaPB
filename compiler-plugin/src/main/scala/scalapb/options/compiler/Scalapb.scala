54df5db0f8092c34e93528294ae61b39dfa39a5d
Nadav Samet <thesamet@gmail.com>
2020-11-14 13:59:14 -0800

    Update deprecation version for scalapb.options.compiler.Scalapb

--- compiler-plugin/src/main/scala/scalapb/options/compiler/Scalapb.scala
+++ compiler-plugin/src/main/scala/scalapb/options/compiler/Scalapb.scala
@@ -3,7 +3,7 @@ package scalapb.options.compiler
 import com.google.protobuf.ExtensionRegistry
 
 object Scalapb {
-  @deprecated("Use scalapb.options.Scalapb.registerAllExtensions", "0.11.0")
+  @deprecated("Use scalapb.options.Scalapb.registerAllExtensions", "0.10.9")
   def registerAllExtensions(registry: ExtensionRegistry) =
     scalapb.options.Scalapb.registerAllExtensions(registry)
 }
