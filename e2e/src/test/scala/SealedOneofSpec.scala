10e0b4d1c1a223d76dfa0f31973c535f511aff84
otto-dev <ignatius.public@gmail.com>
2020-11-15 13:48:29 +1100

    Add end-to-end tests

--- e2e/src/test/scala/SealedOneofSpec.scala
+++ e2e/src/test/scala/SealedOneofSpec.scala
@@ -1,26 +1,44 @@
 import com.thesamet.proto.e2e.sealed_oneof._
 import com.thesamet.proto.e2e.sealed_oneof_in_oneof._
 import com.thesamet.proto.e2e.{sealed_oneof_single_file => f}
+import com.thesamet.proto.e2e.{sealed_oneof_nested_single_file => f2}
 import com.thesamet.proto.e2e.sealed_oneof_extends._
+import com.thesamet.proto.e2e.sealed_oneof_extends_nested.{NestedPlayerBaseTrait, Parent}
+import com.thesamet.proto.e2e.sealed_oneof_in_oneof_nested.Zoo
+import com.thesamet.proto.e2e.sealed_oneof_nested.Nested
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 class SealedOneofSpec extends AnyFlatSpec with Matchers {
 
   val expr = Add(Lit(1), Add(Lit(2), Lit(3)))
-
+  val nestedExpr = Nested.Add(Nested.Lit(1), Nested.Add(Nested.Lit(2), Nested.Lit(3)))
+  
   "Expr.toExprMessage.toExpr" should "roundtrip" in {
     assert(expr == expr.asMessage.toExpr)
   }
 
+  "Nested.Expr.toExprMessage.toExpr" should "roundtrip" in {
+    assert(nestedExpr == nestedExpr.asMessage.toExpr)
+  }
+  
   "ExprMessage.toByteArray" should "work via Expr" in {
     val expr2 = ExprMessage.parseFrom(expr.asMessage.toByteArray).toExpr
     assert(expr == expr2)
   }
 
+  "Nested.ExprMessage.toByteArray" should "work via Expr" in {
+    val expr2 = Nested.ExprMessage.parseFrom(nestedExpr.asMessage.toByteArray).toExpr
+    assert(nestedExpr == expr2)
+  } 
+
   "fields of sealed_oneof type" should "default Empty" in {
     assert(Add() == Add(Expr.Empty, Expr.Empty))
   }
+
+  "fields of nested sealed_oneof type" should "default Empty" in {
+    assert(Nested.Add() == Nested.Add(Nested.Expr.Empty, Nested.Expr.Empty))
+  } 
 
   "fields of repeated sealed_oneof type" should "work like normal" in {
     val programs =
@@ -29,13 +47,20 @@ class SealedOneofSpec extends AnyFlatSpec with Matchers {
     assert(programs == programs2)
   }
 
+  "fields of repeated nested sealed_oneof type" should "work like normal" in {
+    val programs =
+      Nested.Programs(programs = List(nestedExpr, nestedExpr), optionalExpr = nestedExpr, exprMap = Map("44" -> nestedExpr))
+    val programs2 = Nested.Programs.parseFrom(programs.toByteArray)
+    assert(programs == programs2)
+  } 
+
   trait UnsealedExpr
 
   "Expr" should "be sealed" in {
     assertCompiles("class Foo extends UnsealedExpr")
     assertTypeError("class Foo extends Expr")
   }
-
+  
   "single_file=true" should "work with sealed_oneof" in {
     val fexpr = f.Add(f.Lit(1), f.Add(f.Lit(2), f.Lit(3)))
     assert(
@@ -43,6 +68,14 @@ class SealedOneofSpec extends AnyFlatSpec with Matchers {
         expr.asMessage.toProtoString
     )
   }
+
+  "single_file=true" should "work with nested sealed_oneof" in {
+    val fexpr = f2.Nested.Add(f2.Nested.Lit(1), f2.Nested.Add(f2.Nested.Lit(2), f2.Nested.Lit(3)))
+    assert(
+      fexpr.asMessage.toProtoString ==
+          expr.asMessage.toProtoString
+    )
+  } 
 
   "sealed_oneof message" should "work as a oneof case of another message" in {
     // Exhaustive matching of all possible cases
@@ -64,6 +97,27 @@ class SealedOneofSpec extends AnyFlatSpec with Matchers {
                      |}
       """.stripMargin)
   }
+  
+   "nested sealed_oneof message" should "work as a oneof case of another message" in {
+    // Exhaustive matching of all possible cases
+    assertCompiles("""
+                     |Zoo.Animal.defaultInstance.value match {
+                     |  case Zoo.Animal.Value.Empty =>
+                     |  case Zoo.Animal.Value.Mammal(v) =>
+                     |    v match {
+                     |      case Zoo.Mammal.Empty =>
+                     |      case Zoo.Dog(_) =>
+                     |      case Zoo.Cat(_) =>
+                     |    }
+                     |  case Zoo.Animal.Value.Bird(v) =>
+                     |    v match {
+                     |      case Zoo.Bird.Empty =>
+                     |      case Zoo.Eagle(_) =>
+                     |      case Zoo.Sparrow(_) =>
+                     |    }
+                     |}
+      """.stripMargin)
+  } 
 
   "messages using sealed_oneof_extends" should "all extend from custom user trait" in {
     PlayerShim.defaultInstance mustBe a[PlayerBaseTrait]
@@ -71,6 +125,13 @@ class SealedOneofSpec extends AnyFlatSpec with Matchers {
     SoccerPlayer() mustBe a[PlayerBaseTrait]
     SoccerPlayer() mustBe a[scalapb.GeneratedSealedOneof]
   }
+
+  "nested messages using sealed_oneof_extends" should "all extend from custom user trait" in {
+    Parent.PlayerShim.defaultInstance mustBe a[NestedPlayerBaseTrait]
+    Parent.BasketBallPlayer() mustBe a[NestedPlayerBaseTrait]
+    Parent.SoccerPlayer() mustBe a[NestedPlayerBaseTrait]
+    Parent.SoccerPlayer() mustBe a[scalapb.GeneratedSealedOneof]
+  } 
 
   "asNonEmpty" should "return Some or None" in {
     expr.asNonEmpty must be(Some(expr))
