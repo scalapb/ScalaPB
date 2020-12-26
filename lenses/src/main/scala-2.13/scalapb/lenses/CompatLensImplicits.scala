10ae7492b6c8f5d30c5e7e022ddfefaf3c0eddbb
George Leung <phiSgr@users.noreply.github.com>
2020-12-26 13:47:22 +0800

    remove useless =:= (#1017)

--- lenses/src/main/scala-2.13/scalapb/lenses/CompatLensImplicits.scala
+++ lenses/src/main/scala-2.13/scalapb/lenses/CompatLensImplicits.scala
@@ -6,24 +6,23 @@ object CompatLensImplicits {
 
   /** Implicit that adds some syntactic sugar if our lens watches a Seq-like collection. */
   class SeqLikeLens[U, A, CC[A] <: SeqOps[A, CC, CC[A]]](val lens: Lens[U, CC[A]]) extends AnyVal {
-    type C = CC[A]
-    private def field(getter: C => A)(setter: (C, A) => C): Lens[U, A] =
-      lens.compose[A](Lens[C, A](getter)(setter))
+    private def field(getter: CC[A] => A)(setter: (CC[A], A) => CC[A]): Lens[U, A] =
+      lens.compose[A](Lens[CC[A], A](getter)(setter))
 
-    def apply(i: Int)(implicit ev: CC[A] =:= C): Lens[U, A] =
+    def apply(i: Int): Lens[U, A] =
       field(_.apply(i))((c, v) => c.updated(i, v))
 
-    def head(implicit ev: CC[A] =:= C): Lens[U, A] = apply(0)
+    def head: Lens[U, A] = apply(0)
 
-    def last(implicit ev: CC[A] =:= C): Lens[U, A] =
+    def last: Lens[U, A] =
       field(_.last)((c, v) => c.updated(c.size - 1, v))
 
-    def :+=(item: A)(implicit ev: CC[A] =:= C) = lens.modify(_ :+ item)
+    def :+=(item: A) = lens.modify(_ :+ item)
 
-    def :++=(item: IterableOnce[A])(implicit ev: CC[A] =:= C) =
+    def :++=(item: IterableOnce[A]) =
       lens.modify(_ ++ item)
 
-    def foreach(f: Lens[A, A] => Mutation[A])(implicit ev: CC[A] =:= C): Mutation[U] =
+    def foreach(f: Lens[A, A] => Mutation[A]): Mutation[U] =
       lens.modify(s =>
         s.map { (m: A) =>
           val field: Lens[A, A] = Lens.unit[A]
@@ -36,14 +35,12 @@ object CompatLensImplicits {
   /** Implicit that adds some syntactic sugar if our lens watches a Set-like collection. */
   class SetLens[U, A, CC[A] <: collection.immutable.SetOps[A, CC, CC[A]]](val lens: Lens[U, CC[A]])
       extends AnyVal {
-    type C = CC[A]
-
     def :+=(item: A) = lens.modify(_ + item)
 
-    def :++=(item: scala.collection.IterableOnce[A])(implicit ev: CC[A] =:= C) =
+    def :++=(item: scala.collection.IterableOnce[A]) =
       lens.modify(_ ++ item)
 
-    def foreach(f: Lens[A, A] => Mutation[A])(implicit ev: CC[A] =:= C): Mutation[U] =
+    def foreach(f: Lens[A, A] => Mutation[A]): Mutation[U] =
       lens.modify(s =>
         s.map { (m: A) =>
           val field: Lens[A, A] = Lens.unit[A]
