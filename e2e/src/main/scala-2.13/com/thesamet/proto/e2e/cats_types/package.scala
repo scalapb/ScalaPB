2b2fb4215ed446b4d5733e68a8d2099a7db9b485
Nadav Samet <thesamet@gmail.com>
2020-12-29 13:23:58 -0800

    Remove unnecesesary Double/Float implicit ordering

--- e2e/src/main/scala-2.13/com/thesamet/proto/e2e/cats_types/package.scala
+++
@@ -1,9 +0,0 @@
-package com.thesamet.proto.e2e
-
-package object cats_types {
-  implicit val floatOrdering  = Ordering.Float.TotalOrdering
-  implicit val doubleOrdering = Ordering.Double.TotalOrdering
-
-  implicit val enumOrdering: Ordering[com.thesamet.proto.e2e.collection_types.Enum] =
-    Ordering.fromLessThan((x, y) => x.value < y.value)
-}
