c5dd9ff2db0d4a307187ce0d801a40e36e4163b7
Scala Steward <me@scala-steward.org>
2021-02-03 04:43:06 +0100

    Update silencer-lib, silencer-plugin to 1.7.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object versions {
     val grpc                 = "1.35.0"
     val protobuf             = "3.13.0"
-    val silencer             = "1.7.1"
+    val silencer             = "1.7.2"
     val collectionCompat     = "2.4.1"
     val coursier             = "2.0.9"
     val protocGen            = "0.9.2"
