39138937eac8b925466cc6975c40aff240a0a9e8
kenji yoshida <6b656e6a69@gmail.com>
2021-02-03 11:14:46 +0900

    scala-native (#1054)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val grpc                 = "1.35.0"
     val protobuf             = "3.13.0"
     val silencer             = "1.7.1"
-    val collectionCompat     = "2.3.2"
+    val collectionCompat     = "2.4.1"
     val coursier             = "2.0.9"
     val protocGen            = "0.9.2"
     val protobufRuntimeScala = "0.8.7"
@@ -25,7 +25,7 @@ object Dependencies {
     val utest                   = "0.7.4"
   }
 
-  val Scala212 = "2.12.12"
+  val Scala212 = "2.12.13"
 
   val Scala213 = "2.13.4"
 
