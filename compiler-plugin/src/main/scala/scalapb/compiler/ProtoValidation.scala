9753f4a9d9d510b8609f63c24d2a619aa49b2289
Nadav Samet <thesamet@gmail.com>
2021-03-02 16:57:44 -0800

    Return error message when sealed oneof cases are on a different file.
    See #1079

--- compiler-plugin/src/main/scala/scalapb/compiler/ProtoValidation.scala
+++ compiler-plugin/src/main/scala/scalapb/compiler/ProtoValidation.scala
@@ -72,6 +72,11 @@ class ProtoValidation(implicits: DescriptorImplicits) {
           s"${m.getFullName}.${field.getName}: sealed oneofs must be in the same containing message (if any) as all case messages"
         )
       }
+      fields.find(_.getMessageType.getFile != m.getFile()).foreach { field =>
+        throw new GeneratorException(
+          s"${m.getFullName}.${field.getName}: all sealed oneof cases must be defined in the same file as the sealed oneof field."
+        )
+      }
       val distinctTypes = fields.map(_.getMessageType).toSet
       if (distinctTypes.size != fields.size) {
         throw new GeneratorException(
