fd4868b82bae9db23d2f23ddea6dd12e6d434554
piyo7 <piyo7@users.noreply.github.com>
2021-03-25 15:07:32 +0900

    Indent TimstampsSpec with 2 spaces

--- e2e/src/test/scalajvm/TimestampsSpec.scala
+++ e2e/src/test/scalajvm/TimestampsSpec.scala
@@ -6,52 +6,53 @@ import com.google.protobuf.timestamp.Timestamp
 import com.google.protobuf.duration.Duration
 
 class TimestampsSpec extends AnyFlatSpec with Matchers {
-    "Timestamps and Duration" should "have implicit ordering" in {
-        implicitly[Ordering[Timestamp]]
-        implicitly[Ordering[Duration]]
-    }
-    "Timestamps and Duration" should "be convertible for their java.time counterparts via implicit conversions" in {
-        import scalapb.TimestampConverters._
-        import scalapb.DurationConverters._
+  "Timestamps and Duration" should "have implicit ordering" in {
+    implicitly[Ordering[Timestamp]]
+    implicitly[Ordering[Duration]]
+  }
 
-        val t1: Instant = Instant.parse("2018-11-30T18:35:24.123Z")
-        val t2: Instant = Instant.parse("2018-11-30T18:35:25.456Z")
+  "Timestamps and Duration" should "be convertible for their java.time counterparts via implicit conversions" in {
+    import scalapb.TimestampConverters._
+    import scalapb.DurationConverters._
 
-        val timestamp: Timestamp = t1
-        timestamp must be(Timestamp(1543602924, 123000000))
-        (timestamp: Instant) must be(t1)
+    val t1: Instant = Instant.parse("2018-11-30T18:35:24.123Z")
+    val t2: Instant = Instant.parse("2018-11-30T18:35:25.456Z")
 
-        val duration: Duration = jtDuration.between(t1, t2)
-        duration must be(Duration(1, 333000000))
-        (duration: jtDuration) must be(jtDuration.between(t1, t2))
-    }
+    val timestamp: Timestamp = t1
+    timestamp must be(Timestamp(1543602924, 123000000))
+    (timestamp: Instant) must be(t1)
 
-    "Timestamps and Duration" can "be convertible for their java.time counterparts via helpers" in {
-        val t1: Instant = Instant.parse("2018-11-30T18:35:24.123Z")
-        val t2: Instant = Instant.parse("2018-11-30T18:35:25.456Z")
+    val duration: Duration = jtDuration.between(t1, t2)
+    duration must be(Duration(1, 333000000))
+    (duration: jtDuration) must be(jtDuration.between(t1, t2))
+  }
 
-        val timestamp: Timestamp = Timestamp(t1)
-        timestamp must be(Timestamp(1543602924, 123000000))
-        timestamp.asJavaInstant must be(t1)
+  "Timestamps and Duration" can "be convertible for their java.time counterparts via helpers" in {
+    val t1: Instant = Instant.parse("2018-11-30T18:35:24.123Z")
+    val t2: Instant = Instant.parse("2018-11-30T18:35:25.456Z")
 
-        val duration: Duration = Duration(jtDuration.between(t1, t2))
-        duration must be(Duration(1, 333000000))
-        duration.asJavaDuration must be(jtDuration.between(t1, t2))
-    }
+    val timestamp: Timestamp = Timestamp(t1)
+    timestamp must be(Timestamp(1543602924, 123000000))
+    timestamp.asJavaInstant must be(t1)
 
-    "Durations" should "be valid when their seconds and nanos are in defined range and have same sign" in {
-        Duration.isValid(Duration(315576000001L, 0)) must be(false)
-        Duration.isValid(Duration(315576000000L, 999999999)) must be(true)
-        Duration.isValid(Duration(315576000000L, 0)) must be(true)
-        Duration.isValid(Duration(315576000000L, -999999999)) must be(false)
-        Duration.isValid(Duration(0, 1000000000)) must be(false)
-        Duration.isValid(Duration(0, 999999999)) must be(true)
-        Duration.isValid(Duration(0, 0)) must be(true)
-        Duration.isValid(Duration(0, -999999999)) must be(true)
-        Duration.isValid(Duration(0, -1000000000)) must be(false)
-        Duration.isValid(Duration(-315576000000L, 999999999)) must be(false)
-        Duration.isValid(Duration(-315576000000L, 0)) must be(true)
-        Duration.isValid(Duration(-315576000000L, -999999999)) must be(true)
-        Duration.isValid(Duration(-315576000001L, 0)) must be(false)
-    }
+    val duration: Duration = Duration(jtDuration.between(t1, t2))
+    duration must be(Duration(1, 333000000))
+    duration.asJavaDuration must be(jtDuration.between(t1, t2))
+  }
+
+  "Durations" should "be valid when their seconds and nanos are in defined range and have same sign" in {
+    Duration.isValid(Duration(315576000001L, 0)) must be(false)
+    Duration.isValid(Duration(315576000000L, 999999999)) must be(true)
+    Duration.isValid(Duration(315576000000L, 0)) must be(true)
+    Duration.isValid(Duration(315576000000L, -999999999)) must be(false)
+    Duration.isValid(Duration(0, 1000000000)) must be(false)
+    Duration.isValid(Duration(0, 999999999)) must be(true)
+    Duration.isValid(Duration(0, 0)) must be(true)
+    Duration.isValid(Duration(0, -999999999)) must be(true)
+    Duration.isValid(Duration(0, -1000000000)) must be(false)
+    Duration.isValid(Duration(-315576000000L, 999999999)) must be(false)
+    Duration.isValid(Duration(-315576000000L, 0)) must be(true)
+    Duration.isValid(Duration(-315576000000L, -999999999)) must be(true)
+    Duration.isValid(Duration(-315576000001L, 0)) must be(false)
+  }
 }
