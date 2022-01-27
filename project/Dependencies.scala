11cddb6f5ac34ae977395ce7c730d24ea236c40a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-27 04:53:47 +0100

    Update grpc-netty, grpc-protobuf, grpc-stub to 1.44.0 (#1314)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import Keys._
 
 object Dependencies {
   object versions {
-    val grpc                 = "1.43.2"
+    val grpc                 = "1.44.0"
     val protobuf             = "3.19.2"
     val silencer             = "1.7.8"
     val collectionCompat     = "2.6.0"
