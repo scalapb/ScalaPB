161849a0a2d5bbb0356aeb1be72e0bc641db2e80
Scala Steward <me@scala-steward.org>
2022-05-01 01:10:22 +0200

    Reformat with scalafmt 3.5.2

--- e2e/src/test/scala/Issue1354Spec.scala
+++ e2e/src/test/scala/Issue1354Spec.scala
@@ -5,10 +5,10 @@ import org.scalatest.matchers.must.Matchers
 class Issue1354Spec extends AnyFunSpec with Matchers {
 
   it("issue1354") {
-    val hr = HelloRequest(name=Some(Name("name")), nickname = Name("name"))
+    val hr = HelloRequest(name = Some(Name("name")), nickname = Name("name"))
     // nickname is a proto3 optional, but is much to have no_box applied to
     // it.
-    hr.nickname mustBe(a[Name])
+    hr.nickname mustBe (a[Name])
   }
 
 }
