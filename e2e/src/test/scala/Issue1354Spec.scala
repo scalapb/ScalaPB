14a4c3993425a68a4c7a89b2f165af0afe309d81
Nadav Samet <thesamet@gmail.com>
2022-05-08 08:32:38 -0700

    Fix typo in comment

--- e2e/src/test/scala/Issue1354Spec.scala
+++ e2e/src/test/scala/Issue1354Spec.scala
@@ -6,7 +6,7 @@ class Issue1354Spec extends AnyFunSpec with Matchers {
 
   it("issue1354") {
     val hr = HelloRequest(name = Some(Name("name")), nickname = Name("name"))
-    // nickname is a proto3 optional, but is much to have no_box applied to
+    // nickname is a proto3 optional, but is matched to have no_box applied to
     // it.
     hr.nickname mustBe (a[Name])
   }
