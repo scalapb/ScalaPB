d31a91f5ea88791eff14c482c6dff5a088eb7c43
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-31 20:00:49 +0200

    Update scalatest to 3.2.13 (#1393)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val cats                    = "2.6.1"
     val mockito                 = "4.6.1"
     val munit                   = "0.7.29"
-    val scalaTest               = "3.2.11"
+    val scalaTest               = "3.2.13"
     val scalaTestPlusMockito    = "3.1.0.0"
     val scalaTestPlusScalaCheck = "3.2.11.0"
     val utest                   = "0.7.10"
