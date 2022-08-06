400716c8d28422280325e174eb58b0f6497d1203
jodersky <jakob@odersky.com>
2022-08-06 18:38:36 +0200

    Build for Scala Native, Scala 3 (#1396)
    Since 0.4.4, Scala Native supports Scala 3.
    
    The update to the munit testing library was necessary because the 0.x
    series does not support Scala Native on 3 [1].
    
    [1]: https://github.com/scalameta/munit/pull/511

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val annotationApi           = "1.3.2"
     val cats                    = "2.6.1"
     val mockito                 = "4.6.1"
-    val munit                   = "0.7.29"
+    val munit                   = "1.0.0-M6"
     val scalaTest               = "3.2.13"
     val scalaTestPlusMockito    = "3.1.0.0"
     val scalaTestPlusScalaCheck = "3.2.11.0"
