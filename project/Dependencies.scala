c12cf6664f774d68555bd054022fbd6b77091240
Felix Sonntag <felix.sonntag@outlook.com>
2022-12-22 20:04:26 +0700

    Update to latest grpc version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import Keys._
 
 object Dependencies {
   object versions {
-    val grpc                 = "1.47.1"
-    val protobuf             = "3.19.6"
+    val grpc                 = "1.51.1"
+    val protobuf             = "3.21.12"
     val silencer             = "1.7.12"
     val collectionCompat     = "2.9.0"
     val coursier             = "2.0.16"
