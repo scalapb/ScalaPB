c2c345ff37045166fbf7224c3eaa3da84d3d9513
Scala Steward <scala_steward@virtuslab.com>
2023-03-28 18:38:28 +0000

    Reformat with scalafmt 3.7.3
    Executed command: scalafmt --non-interactive

--- docs/src/main/scala/generated/scalapb/perf/protos/Enum.scala
+++ docs/src/main/scala/generated/scalapb/perf/protos/Enum.scala
@@ -9,67 +9,73 @@ package scalapb.perf.protos
 final case class Enum(
     color: scalapb.perf.protos.Color = scalapb.perf.protos.Color.UNKNOWN,
     unknownFields: _root_.scalapb.UnknownFieldSet = _root_.scalapb.UnknownFieldSet.empty
-    ) extends scalapb.GeneratedMessage with scalapb.lenses.Updatable[Enum] {
-    @transient
-    private[this] var __serializedSizeMemoized: _root_.scala.Int = 0
-    private[this] def __computeSerializedSize(): _root_.scala.Int = {
-      var __size = 0
-      
-      {
-        val __value = color.value
-        if (__value != 0) {
-          __size += _root_.com.google.protobuf.CodedOutputStream.computeEnumSize(1, __value)
-        }
-      };
-      __size += unknownFields.serializedSize
-      __size
-    }
-    override def serializedSize: _root_.scala.Int = {
-      var __size = __serializedSizeMemoized
-      if (__size == 0) {
-        __size = __computeSerializedSize() + 1
-        __serializedSizeMemoized = __size
+) extends scalapb.GeneratedMessage
+    with scalapb.lenses.Updatable[Enum] {
+  @transient
+  private[this] var __serializedSizeMemoized: _root_.scala.Int = 0
+  private[this] def __computeSerializedSize(): _root_.scala.Int = {
+    var __size = 0
+
+    {
+      val __value = color.value
+      if (__value != 0) {
+        __size += _root_.com.google.protobuf.CodedOutputStream.computeEnumSize(1, __value)
       }
-      __size - 1
-      
+    };
+    __size += unknownFields.serializedSize
+    __size
+  }
+  override def serializedSize: _root_.scala.Int = {
+    var __size = __serializedSizeMemoized
+    if (__size == 0) {
+      __size = __computeSerializedSize() + 1
+      __serializedSizeMemoized = __size
     }
-    def writeTo(`_output__`: _root_.com.google.protobuf.CodedOutputStream): _root_.scala.Unit = {
-      {
-        val __v = color.value
-        if (__v != 0) {
-          _output__.writeEnum(1, __v)
-        }
-      };
-      unknownFields.writeTo(_output__)
-    }
-    def withColor(__v: scalapb.perf.protos.Color): Enum = copy(color = __v)
-    def withUnknownFields(__v: _root_.scalapb.UnknownFieldSet) = copy(unknownFields = __v)
-    def discardUnknownFields = copy(unknownFields = _root_.scalapb.UnknownFieldSet.empty)
-    def getFieldByNumber(__fieldNumber: _root_.scala.Int): _root_.scala.Any = {
-      (__fieldNumber: @_root_.scala.unchecked) match {
-        case 1 => {
-          val __t = color.javaValueDescriptor
-          if (__t.getNumber() != 0) __t else null
-        }
+    __size - 1
+
+  }
+  def writeTo(`_output__`: _root_.com.google.protobuf.CodedOutputStream): _root_.scala.Unit = {
+    {
+      val __v = color.value
+      if (__v != 0) {
+        _output__.writeEnum(1, __v)
       }
-    }
-    def getField(__field: _root_.scalapb.descriptors.FieldDescriptor): _root_.scalapb.descriptors.PValue = {
-      _root_.scala.Predef.require(__field.containingMessage eq companion.scalaDescriptor)
-      (__field.number: @_root_.scala.unchecked) match {
-        case 1 => _root_.scalapb.descriptors.PEnum(color.scalaValueDescriptor)
+    };
+    unknownFields.writeTo(_output__)
+  }
+  def withColor(__v: scalapb.perf.protos.Color): Enum        = copy(color = __v)
+  def withUnknownFields(__v: _root_.scalapb.UnknownFieldSet) = copy(unknownFields = __v)
+  def discardUnknownFields = copy(unknownFields = _root_.scalapb.UnknownFieldSet.empty)
+  def getFieldByNumber(__fieldNumber: _root_.scala.Int): _root_.scala.Any = {
+    (__fieldNumber: @ _root_.scala.unchecked) match {
+      case 1 => {
+        val __t = color.javaValueDescriptor
+        if (__t.getNumber() != 0) __t else null
       }
     }
-    def toProtoString: _root_.scala.Predef.String = _root_.scalapb.TextFormat.printToUnicodeString(this)
-    def companion: scalapb.perf.protos.Enum.type = scalapb.perf.protos.Enum
-    // @@protoc_insertion_point(GeneratedMessage[scalapb.perf.Enum])
+  }
+  def getField(
+      __field: _root_.scalapb.descriptors.FieldDescriptor
+  ): _root_.scalapb.descriptors.PValue = {
+    _root_.scala.Predef.require(__field.containingMessage eq companion.scalaDescriptor)
+    (__field.number: @ _root_.scala.unchecked) match {
+      case 1 => _root_.scalapb.descriptors.PEnum(color.scalaValueDescriptor)
+    }
+  }
+  def toProtoString: _root_.scala.Predef.String =
+    _root_.scalapb.TextFormat.printToUnicodeString(this)
+  def companion: scalapb.perf.protos.Enum.type = scalapb.perf.protos.Enum
+  // @@protoc_insertion_point(GeneratedMessage[scalapb.perf.Enum])
 }
 
 object Enum extends scalapb.GeneratedMessageCompanion[scalapb.perf.protos.Enum] {
   implicit def messageCompanion: scalapb.GeneratedMessageCompanion[scalapb.perf.protos.Enum] = this
-  def parseFrom(`_input__`: _root_.com.google.protobuf.CodedInputStream): scalapb.perf.protos.Enum = {
+  def parseFrom(
+      `_input__`: _root_.com.google.protobuf.CodedInputStream
+  ): scalapb.perf.protos.Enum = {
     var __color: scalapb.perf.protos.Color = scalapb.perf.protos.Color.UNKNOWN
-    var `_unknownFields__`: _root_.scalapb.UnknownFieldSet.Builder = null
-    var _done__ = false
+    var `_unknownFields__` : _root_.scalapb.UnknownFieldSet.Builder = null
+    var _done__                                                     = false
     while (!_done__) {
       val _tag__ = _input__.readTag()
       _tag__ match {
@@ -84,36 +90,59 @@ object Enum extends scalapb.GeneratedMessageCompanion[scalapb.perf.protos.Enum] 
       }
     }
     scalapb.perf.protos.Enum(
-        color = __color,
-        unknownFields = if (_unknownFields__ == null) _root_.scalapb.UnknownFieldSet.empty else _unknownFields__.result()
+      color = __color,
+      unknownFields =
+        if (_unknownFields__ == null) _root_.scalapb.UnknownFieldSet.empty
+        else _unknownFields__.result()
     )
   }
-  implicit def messageReads: _root_.scalapb.descriptors.Reads[scalapb.perf.protos.Enum] = _root_.scalapb.descriptors.Reads{
-    case _root_.scalapb.descriptors.PMessage(__fieldsMap) =>
-      _root_.scala.Predef.require(__fieldsMap.keys.forall(_.containingMessage eq scalaDescriptor), "FieldDescriptor does not match message type.")
-      scalapb.perf.protos.Enum(
-        color = scalapb.perf.protos.Color.fromValue(__fieldsMap.get(scalaDescriptor.findFieldByNumber(1).get).map(_.as[_root_.scalapb.descriptors.EnumValueDescriptor]).getOrElse(scalapb.perf.protos.Color.UNKNOWN.scalaValueDescriptor).number)
-      )
-    case _ => throw new RuntimeException("Expected PMessage")
-  }
-  def javaDescriptor: _root_.com.google.protobuf.Descriptors.Descriptor = ProtosProto.javaDescriptor.getMessageTypes().get(2)
-  def scalaDescriptor: _root_.scalapb.descriptors.Descriptor = ProtosProto.scalaDescriptor.messages(2)
-  def messageCompanionForFieldNumber(__number: _root_.scala.Int): _root_.scalapb.GeneratedMessageCompanion[_] = throw new MatchError(__number)
-  lazy val nestedMessagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] = Seq.empty
-  def enumCompanionForFieldNumber(__fieldNumber: _root_.scala.Int): _root_.scalapb.GeneratedEnumCompanion[_] = {
-    (__fieldNumber: @_root_.scala.unchecked) match {
+  implicit def messageReads: _root_.scalapb.descriptors.Reads[scalapb.perf.protos.Enum] =
+    _root_.scalapb.descriptors.Reads {
+      case _root_.scalapb.descriptors.PMessage(__fieldsMap) =>
+        _root_.scala.Predef.require(
+          __fieldsMap.keys.forall(_.containingMessage eq scalaDescriptor),
+          "FieldDescriptor does not match message type."
+        )
+        scalapb.perf.protos.Enum(
+          color = scalapb.perf.protos.Color.fromValue(
+            __fieldsMap
+              .get(scalaDescriptor.findFieldByNumber(1).get)
+              .map(_.as[_root_.scalapb.descriptors.EnumValueDescriptor])
+              .getOrElse(scalapb.perf.protos.Color.UNKNOWN.scalaValueDescriptor)
+              .number
+          )
+        )
+      case _ => throw new RuntimeException("Expected PMessage")
+    }
+  def javaDescriptor: _root_.com.google.protobuf.Descriptors.Descriptor =
+    ProtosProto.javaDescriptor.getMessageTypes().get(2)
+  def scalaDescriptor: _root_.scalapb.descriptors.Descriptor =
+    ProtosProto.scalaDescriptor.messages(2)
+  def messageCompanionForFieldNumber(
+      __number: _root_.scala.Int
+  ): _root_.scalapb.GeneratedMessageCompanion[_] = throw new MatchError(__number)
+  lazy val nestedMessagesCompanions
+      : Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] =
+    Seq.empty
+  def enumCompanionForFieldNumber(
+      __fieldNumber: _root_.scala.Int
+  ): _root_.scalapb.GeneratedEnumCompanion[_] = {
+    (__fieldNumber: @ _root_.scala.unchecked) match {
       case 1 => scalapb.perf.protos.Color
     }
   }
   lazy val defaultInstance = scalapb.perf.protos.Enum(
     color = scalapb.perf.protos.Color.UNKNOWN
   )
-  implicit class EnumLens[UpperPB](_l: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Enum]) extends _root_.scalapb.lenses.ObjectLens[UpperPB, scalapb.perf.protos.Enum](_l) {
-    def color: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Color] = field(_.color)((c_, f_) => c_.copy(color = f_))
+  implicit class EnumLens[UpperPB](
+      _l: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Enum]
+  ) extends _root_.scalapb.lenses.ObjectLens[UpperPB, scalapb.perf.protos.Enum](_l) {
+    def color: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Color] =
+      field(_.color)((c_, f_) => c_.copy(color = f_))
   }
   final val COLOR_FIELD_NUMBER = 1
   def of(
-    color: scalapb.perf.protos.Color
+      color: scalapb.perf.protos.Color
   ): _root_.scalapb.perf.protos.Enum = _root_.scalapb.perf.protos.Enum(
     color
   )
--- docs/src/main/scala/generated/scalapb/perf/protos/SimpleMessage.scala
+++ docs/src/main/scala/generated/scalapb/perf/protos/SimpleMessage.scala
@@ -12,127 +12,134 @@ final case class SimpleMessage(
     k: _root_.com.google.protobuf.ByteString = _root_.com.google.protobuf.ByteString.EMPTY,
     color: scalapb.perf.protos.Color = scalapb.perf.protos.Color.UNKNOWN,
     unknownFields: _root_.scalapb.UnknownFieldSet = _root_.scalapb.UnknownFieldSet.empty
-    ) extends scalapb.GeneratedMessage with scalapb.lenses.Updatable[SimpleMessage] {
-    @transient
-    private[this] var __serializedSizeMemoized: _root_.scala.Int = 0
-    private[this] def __computeSerializedSize(): _root_.scala.Int = {
-      var __size = 0
-      
-      {
-        val __value = i
-        if (__value != 0) {
-          __size += _root_.com.google.protobuf.CodedOutputStream.computeInt32Size(1, __value)
-        }
-      };
-      
-      {
-        val __value = j
-        if (__value != 0) {
-          __size += _root_.com.google.protobuf.CodedOutputStream.computeInt32Size(2, __value)
-        }
-      };
-      
-      {
-        val __value = k
-        if (!__value.isEmpty) {
-          __size += _root_.com.google.protobuf.CodedOutputStream.computeBytesSize(3, __value)
-        }
-      };
-      
-      {
-        val __value = color.value
-        if (__value != 0) {
-          __size += _root_.com.google.protobuf.CodedOutputStream.computeEnumSize(4, __value)
-        }
-      };
-      __size += unknownFields.serializedSize
-      __size
-    }
-    override def serializedSize: _root_.scala.Int = {
-      var __size = __serializedSizeMemoized
-      if (__size == 0) {
-        __size = __computeSerializedSize() + 1
-        __serializedSizeMemoized = __size
+) extends scalapb.GeneratedMessage
+    with scalapb.lenses.Updatable[SimpleMessage] {
+  @transient
+  private[this] var __serializedSizeMemoized: _root_.scala.Int = 0
+  private[this] def __computeSerializedSize(): _root_.scala.Int = {
+    var __size = 0
+
+    {
+      val __value = i
+      if (__value != 0) {
+        __size += _root_.com.google.protobuf.CodedOutputStream.computeInt32Size(1, __value)
       }
-      __size - 1
-      
-    }
-    def writeTo(`_output__`: _root_.com.google.protobuf.CodedOutputStream): _root_.scala.Unit = {
-      {
-        val __v = i
-        if (__v != 0) {
-          _output__.writeInt32(1, __v)
-        }
-      };
-      {
-        val __v = j
-        if (__v != 0) {
-          _output__.writeInt32(2, __v)
-        }
-      };
-      {
-        val __v = k
-        if (!__v.isEmpty) {
-          _output__.writeBytes(3, __v)
-        }
-      };
-      {
-        val __v = color.value
-        if (__v != 0) {
-          _output__.writeEnum(4, __v)
-        }
-      };
-      unknownFields.writeTo(_output__)
-    }
-    def withI(__v: _root_.scala.Int): SimpleMessage = copy(i = __v)
-    def withJ(__v: _root_.scala.Int): SimpleMessage = copy(j = __v)
-    def withK(__v: _root_.com.google.protobuf.ByteString): SimpleMessage = copy(k = __v)
-    def withColor(__v: scalapb.perf.protos.Color): SimpleMessage = copy(color = __v)
-    def withUnknownFields(__v: _root_.scalapb.UnknownFieldSet) = copy(unknownFields = __v)
-    def discardUnknownFields = copy(unknownFields = _root_.scalapb.UnknownFieldSet.empty)
-    def getFieldByNumber(__fieldNumber: _root_.scala.Int): _root_.scala.Any = {
-      (__fieldNumber: @_root_.scala.unchecked) match {
-        case 1 => {
-          val __t = i
-          if (__t != 0) __t else null
-        }
-        case 2 => {
-          val __t = j
-          if (__t != 0) __t else null
-        }
-        case 3 => {
-          val __t = k
-          if (__t != _root_.com.google.protobuf.ByteString.EMPTY) __t else null
-        }
-        case 4 => {
-          val __t = color.javaValueDescriptor
-          if (__t.getNumber() != 0) __t else null
-        }
+    };
+
+    {
+      val __value = j
+      if (__value != 0) {
+        __size += _root_.com.google.protobuf.CodedOutputStream.computeInt32Size(2, __value)
       }
+    };
+
+    {
+      val __value = k
+      if (!__value.isEmpty) {
+        __size += _root_.com.google.protobuf.CodedOutputStream.computeBytesSize(3, __value)
+      }
+    };
+
+    {
+      val __value = color.value
+      if (__value != 0) {
+        __size += _root_.com.google.protobuf.CodedOutputStream.computeEnumSize(4, __value)
+      }
+    };
+    __size += unknownFields.serializedSize
+    __size
+  }
+  override def serializedSize: _root_.scala.Int = {
+    var __size = __serializedSizeMemoized
+    if (__size == 0) {
+      __size = __computeSerializedSize() + 1
+      __serializedSizeMemoized = __size
     }
-    def getField(__field: _root_.scalapb.descriptors.FieldDescriptor): _root_.scalapb.descriptors.PValue = {
-      _root_.scala.Predef.require(__field.containingMessage eq companion.scalaDescriptor)
-      (__field.number: @_root_.scala.unchecked) match {
-        case 1 => _root_.scalapb.descriptors.PInt(i)
-        case 2 => _root_.scalapb.descriptors.PInt(j)
-        case 3 => _root_.scalapb.descriptors.PByteString(k)
-        case 4 => _root_.scalapb.descriptors.PEnum(color.scalaValueDescriptor)
+    __size - 1
+
+  }
+  def writeTo(`_output__`: _root_.com.google.protobuf.CodedOutputStream): _root_.scala.Unit = {
+    {
+      val __v = i
+      if (__v != 0) {
+        _output__.writeInt32(1, __v)
+      }
+    };
+    {
+      val __v = j
+      if (__v != 0) {
+        _output__.writeInt32(2, __v)
+      }
+    };
+    {
+      val __v = k
+      if (!__v.isEmpty) {
+        _output__.writeBytes(3, __v)
+      }
+    };
+    {
+      val __v = color.value
+      if (__v != 0) {
+        _output__.writeEnum(4, __v)
+      }
+    };
+    unknownFields.writeTo(_output__)
+  }
+  def withI(__v: _root_.scala.Int): SimpleMessage                      = copy(i = __v)
+  def withJ(__v: _root_.scala.Int): SimpleMessage                      = copy(j = __v)
+  def withK(__v: _root_.com.google.protobuf.ByteString): SimpleMessage = copy(k = __v)
+  def withColor(__v: scalapb.perf.protos.Color): SimpleMessage         = copy(color = __v)
+  def withUnknownFields(__v: _root_.scalapb.UnknownFieldSet)           = copy(unknownFields = __v)
+  def discardUnknownFields = copy(unknownFields = _root_.scalapb.UnknownFieldSet.empty)
+  def getFieldByNumber(__fieldNumber: _root_.scala.Int): _root_.scala.Any = {
+    (__fieldNumber: @ _root_.scala.unchecked) match {
+      case 1 => {
+        val __t = i
+        if (__t != 0) __t else null
+      }
+      case 2 => {
+        val __t = j
+        if (__t != 0) __t else null
+      }
+      case 3 => {
+        val __t = k
+        if (__t != _root_.com.google.protobuf.ByteString.EMPTY) __t else null
+      }
+      case 4 => {
+        val __t = color.javaValueDescriptor
+        if (__t.getNumber() != 0) __t else null
       }
     }
-    def toProtoString: _root_.scala.Predef.String = _root_.scalapb.TextFormat.printToUnicodeString(this)
-    def companion: scalapb.perf.protos.SimpleMessage.type = scalapb.perf.protos.SimpleMessage
-    // @@protoc_insertion_point(GeneratedMessage[scalapb.perf.SimpleMessage])
+  }
+  def getField(
+      __field: _root_.scalapb.descriptors.FieldDescriptor
+  ): _root_.scalapb.descriptors.PValue = {
+    _root_.scala.Predef.require(__field.containingMessage eq companion.scalaDescriptor)
+    (__field.number: @ _root_.scala.unchecked) match {
+      case 1 => _root_.scalapb.descriptors.PInt(i)
+      case 2 => _root_.scalapb.descriptors.PInt(j)
+      case 3 => _root_.scalapb.descriptors.PByteString(k)
+      case 4 => _root_.scalapb.descriptors.PEnum(color.scalaValueDescriptor)
+    }
+  }
+  def toProtoString: _root_.scala.Predef.String =
+    _root_.scalapb.TextFormat.printToUnicodeString(this)
+  def companion: scalapb.perf.protos.SimpleMessage.type = scalapb.perf.protos.SimpleMessage
+  // @@protoc_insertion_point(GeneratedMessage[scalapb.perf.SimpleMessage])
 }
 
 object SimpleMessage extends scalapb.GeneratedMessageCompanion[scalapb.perf.protos.SimpleMessage] {
-  implicit def messageCompanion: scalapb.GeneratedMessageCompanion[scalapb.perf.protos.SimpleMessage] = this
-  def parseFrom(`_input__`: _root_.com.google.protobuf.CodedInputStream): scalapb.perf.protos.SimpleMessage = {
-    var __i: _root_.scala.Int = 0
-    var __j: _root_.scala.Int = 0
+  implicit def messageCompanion
+      : scalapb.GeneratedMessageCompanion[scalapb.perf.protos.SimpleMessage] = this
+  def parseFrom(
+      `_input__`: _root_.com.google.protobuf.CodedInputStream
+  ): scalapb.perf.protos.SimpleMessage = {
+    var __i: _root_.scala.Int                      = 0
+    var __j: _root_.scala.Int                      = 0
     var __k: _root_.com.google.protobuf.ByteString = _root_.com.google.protobuf.ByteString.EMPTY
-    var __color: scalapb.perf.protos.Color = scalapb.perf.protos.Color.UNKNOWN
-    var `_unknownFields__`: _root_.scalapb.UnknownFieldSet.Builder = null
-    var _done__ = false
+    var __color: scalapb.perf.protos.Color         = scalapb.perf.protos.Color.UNKNOWN
+    var `_unknownFields__` : _root_.scalapb.UnknownFieldSet.Builder = null
+    var _done__                                                     = false
     while (!_done__) {
       val _tag__ = _input__.readTag()
       _tag__ match {
@@ -153,30 +160,59 @@ object SimpleMessage extends scalapb.GeneratedMessageCompanion[scalapb.perf.prot
       }
     }
     scalapb.perf.protos.SimpleMessage(
-        i = __i,
-        j = __j,
-        k = __k,
-        color = __color,
-        unknownFields = if (_unknownFields__ == null) _root_.scalapb.UnknownFieldSet.empty else _unknownFields__.result()
+      i = __i,
+      j = __j,
+      k = __k,
+      color = __color,
+      unknownFields =
+        if (_unknownFields__ == null) _root_.scalapb.UnknownFieldSet.empty
+        else _unknownFields__.result()
     )
   }
-  implicit def messageReads: _root_.scalapb.descriptors.Reads[scalapb.perf.protos.SimpleMessage] = _root_.scalapb.descriptors.Reads{
-    case _root_.scalapb.descriptors.PMessage(__fieldsMap) =>
-      _root_.scala.Predef.require(__fieldsMap.keys.forall(_.containingMessage eq scalaDescriptor), "FieldDescriptor does not match message type.")
-      scalapb.perf.protos.SimpleMessage(
-        i = __fieldsMap.get(scalaDescriptor.findFieldByNumber(1).get).map(_.as[_root_.scala.Int]).getOrElse(0),
-        j = __fieldsMap.get(scalaDescriptor.findFieldByNumber(2).get).map(_.as[_root_.scala.Int]).getOrElse(0),
-        k = __fieldsMap.get(scalaDescriptor.findFieldByNumber(3).get).map(_.as[_root_.com.google.protobuf.ByteString]).getOrElse(_root_.com.google.protobuf.ByteString.EMPTY),
-        color = scalapb.perf.protos.Color.fromValue(__fieldsMap.get(scalaDescriptor.findFieldByNumber(4).get).map(_.as[_root_.scalapb.descriptors.EnumValueDescriptor]).getOrElse(scalapb.perf.protos.Color.UNKNOWN.scalaValueDescriptor).number)
-      )
-    case _ => throw new RuntimeException("Expected PMessage")
-  }
-  def javaDescriptor: _root_.com.google.protobuf.Descriptors.Descriptor = ProtosProto.javaDescriptor.getMessageTypes().get(0)
-  def scalaDescriptor: _root_.scalapb.descriptors.Descriptor = ProtosProto.scalaDescriptor.messages(0)
-  def messageCompanionForFieldNumber(__number: _root_.scala.Int): _root_.scalapb.GeneratedMessageCompanion[_] = throw new MatchError(__number)
-  lazy val nestedMessagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] = Seq.empty
-  def enumCompanionForFieldNumber(__fieldNumber: _root_.scala.Int): _root_.scalapb.GeneratedEnumCompanion[_] = {
-    (__fieldNumber: @_root_.scala.unchecked) match {
+  implicit def messageReads: _root_.scalapb.descriptors.Reads[scalapb.perf.protos.SimpleMessage] =
+    _root_.scalapb.descriptors.Reads {
+      case _root_.scalapb.descriptors.PMessage(__fieldsMap) =>
+        _root_.scala.Predef.require(
+          __fieldsMap.keys.forall(_.containingMessage eq scalaDescriptor),
+          "FieldDescriptor does not match message type."
+        )
+        scalapb.perf.protos.SimpleMessage(
+          i = __fieldsMap
+            .get(scalaDescriptor.findFieldByNumber(1).get)
+            .map(_.as[_root_.scala.Int])
+            .getOrElse(0),
+          j = __fieldsMap
+            .get(scalaDescriptor.findFieldByNumber(2).get)
+            .map(_.as[_root_.scala.Int])
+            .getOrElse(0),
+          k = __fieldsMap
+            .get(scalaDescriptor.findFieldByNumber(3).get)
+            .map(_.as[_root_.com.google.protobuf.ByteString])
+            .getOrElse(_root_.com.google.protobuf.ByteString.EMPTY),
+          color = scalapb.perf.protos.Color.fromValue(
+            __fieldsMap
+              .get(scalaDescriptor.findFieldByNumber(4).get)
+              .map(_.as[_root_.scalapb.descriptors.EnumValueDescriptor])
+              .getOrElse(scalapb.perf.protos.Color.UNKNOWN.scalaValueDescriptor)
+              .number
+          )
+        )
+      case _ => throw new RuntimeException("Expected PMessage")
+    }
+  def javaDescriptor: _root_.com.google.protobuf.Descriptors.Descriptor =
+    ProtosProto.javaDescriptor.getMessageTypes().get(0)
+  def scalaDescriptor: _root_.scalapb.descriptors.Descriptor =
+    ProtosProto.scalaDescriptor.messages(0)
+  def messageCompanionForFieldNumber(
+      __number: _root_.scala.Int
+  ): _root_.scalapb.GeneratedMessageCompanion[_] = throw new MatchError(__number)
+  lazy val nestedMessagesCompanions
+      : Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] =
+    Seq.empty
+  def enumCompanionForFieldNumber(
+      __fieldNumber: _root_.scala.Int
+  ): _root_.scalapb.GeneratedEnumCompanion[_] = {
+    (__fieldNumber: @ _root_.scala.unchecked) match {
       case 4 => scalapb.perf.protos.Color
     }
   }
@@ -186,21 +222,27 @@ object SimpleMessage extends scalapb.GeneratedMessageCompanion[scalapb.perf.prot
     k = _root_.com.google.protobuf.ByteString.EMPTY,
     color = scalapb.perf.protos.Color.UNKNOWN
   )
-  implicit class SimpleMessageLens[UpperPB](_l: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.SimpleMessage]) extends _root_.scalapb.lenses.ObjectLens[UpperPB, scalapb.perf.protos.SimpleMessage](_l) {
-    def i: _root_.scalapb.lenses.Lens[UpperPB, _root_.scala.Int] = field(_.i)((c_, f_) => c_.copy(i = f_))
-    def j: _root_.scalapb.lenses.Lens[UpperPB, _root_.scala.Int] = field(_.j)((c_, f_) => c_.copy(j = f_))
-    def k: _root_.scalapb.lenses.Lens[UpperPB, _root_.com.google.protobuf.ByteString] = field(_.k)((c_, f_) => c_.copy(k = f_))
-    def color: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Color] = field(_.color)((c_, f_) => c_.copy(color = f_))
+  implicit class SimpleMessageLens[UpperPB](
+      _l: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.SimpleMessage]
+  ) extends _root_.scalapb.lenses.ObjectLens[UpperPB, scalapb.perf.protos.SimpleMessage](_l) {
+    def i: _root_.scalapb.lenses.Lens[UpperPB, _root_.scala.Int] =
+      field(_.i)((c_, f_) => c_.copy(i = f_))
+    def j: _root_.scalapb.lenses.Lens[UpperPB, _root_.scala.Int] =
+      field(_.j)((c_, f_) => c_.copy(j = f_))
+    def k: _root_.scalapb.lenses.Lens[UpperPB, _root_.com.google.protobuf.ByteString] =
+      field(_.k)((c_, f_) => c_.copy(k = f_))
+    def color: _root_.scalapb.lenses.Lens[UpperPB, scalapb.perf.protos.Color] =
+      field(_.color)((c_, f_) => c_.copy(color = f_))
   }
-  final val I_FIELD_NUMBER = 1
-  final val J_FIELD_NUMBER = 2
-  final val K_FIELD_NUMBER = 3
+  final val I_FIELD_NUMBER     = 1
+  final val J_FIELD_NUMBER     = 2
+  final val K_FIELD_NUMBER     = 3
   final val COLOR_FIELD_NUMBER = 4
   def of(
-    i: _root_.scala.Int,
-    j: _root_.scala.Int,
-    k: _root_.com.google.protobuf.ByteString,
-    color: scalapb.perf.protos.Color
+      i: _root_.scala.Int,
+      j: _root_.scala.Int,
+      k: _root_.com.google.protobuf.ByteString,
+      color: scalapb.perf.protos.Color
   ): _root_.scalapb.perf.protos.SimpleMessage = _root_.scalapb.perf.protos.SimpleMessage(
     i,
     j,
