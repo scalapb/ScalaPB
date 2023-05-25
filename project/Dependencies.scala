5c7a9ed63f9a3441e5bc1bd8b57c6bdb313b4ebc
Scala Steward <scala_steward@virtuslab.com>
2023-05-25 08:18:44 +0000

    Update scalacheck-1-17 to 3.2.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val munit                   = "1.0.0-M7"
     val scalaTest               = "3.2.15"
     val scalaTestPlusMockito    = "3.1.0.0"
-    val scalaTestPlusScalaCheck = "3.2.15.0"
+    val scalaTestPlusScalaCheck = "3.2.16.0"
     val utest                   = "0.7.10"
   }
 
