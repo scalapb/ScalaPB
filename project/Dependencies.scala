64c18385644dd310b8d1c91f8aa6beb73bf572f5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-11 00:41:07 +0200

    Update munit, munit-scalacheck to 1.0.0-M8 (#1529)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val annotationApi           = "1.3.2"
     val cats                    = "2.6.1"
     val mockito                 = "5.3.1"
-    val munit                   = "1.0.0-M7"
+    val munit                   = "1.0.0-M8"
     val scalaTest               = "3.2.16"
     val scalaTestPlusMockito    = "3.1.0.0"
     val scalaTestPlusScalaCheck = "3.2.16.0"
