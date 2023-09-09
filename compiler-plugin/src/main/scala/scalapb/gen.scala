69ab4a7b68212e8923f187c12fff1771216d3bb1
Alden Torres <aldenml@cmaster.us>
2023-09-09 11:52:16 -0400

    add missing retainSourceCodeInfo in scalapb.gen

--- compiler-plugin/src/main/scala/scalapb/gen.scala
+++ compiler-plugin/src/main/scala/scalapb/gen.scala
@@ -33,7 +33,8 @@ object gen {
       grpc: Boolean = true,
       singleLineToProtoString: Boolean = false,
       asciiFormatToString: Boolean = false,
-      lenses: Boolean = true
+      lenses: Boolean = true,
+      retainSourceCodeInfo: Boolean = false
   ): (SandboxedJvmGenerator, Seq[String]) = {
     val optionsBuilder = Set.newBuilder[GeneratorOption]
     if (flatPackage) {
@@ -53,6 +54,9 @@ object gen {
     }
     if (!lenses) {
       optionsBuilder += NoLenses
+    }
+    if (retainSourceCodeInfo) {
+      optionsBuilder += RetainSourceCodeInfo
     }
     apply(optionsBuilder.result())
   }
