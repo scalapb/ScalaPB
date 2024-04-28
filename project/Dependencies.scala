ddf9a2c7e8eb604dc1e9b072fb52952c04d8e8ba
Nadav Samet <thesamet@gmail.com>
2024-04-28 11:40:25 -0700

    Bump protobuf-scala-runtime to 0.8.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val collectionCompat     = "2.12.0"
     val coursier             = "2.1.9"
     val protocGen            = "0.9.7"
-    val protobufRuntimeScala = "0.8.14"
+    val protobufRuntimeScala = "0.8.15"
     val commonsCodec         = "1.16.1"
 
     // For testing
