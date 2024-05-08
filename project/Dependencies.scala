e38a13b197fc26c177362274e446eaa45521fde2
Scala Steward <scala_steward@virtuslab.com>
2024-05-08 05:25:32 +0000

    Update coursier to 2.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val protobuf             = "4.26.0"
     val silencer             = "1.7.16"
     val collectionCompat     = "2.12.0"
-    val coursier             = "2.1.9"
+    val coursier             = "2.1.10"
     val protocGen            = "0.9.7"
     val protobufRuntimeScala = "0.8.15"
     val commonsCodec         = "1.17.0"
