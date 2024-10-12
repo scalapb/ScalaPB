26997a797ffd68b865601a80d6aa3812fc3b0017
Scala Steward <scala_steward@virtuslab.com>
2024-10-12 17:59:50 +0000

    Update silencer-lib, silencer-plugin to 1.7.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object versions {
     val grpc                 = "1.68.0"
     val protobuf             = "4.28.2"
-    val silencer             = "1.7.17"
+    val silencer             = "1.7.18"
     val collectionCompat     = "2.12.0"
     val coursier             = "2.1.13"
     val protocGen            = "0.9.7"
