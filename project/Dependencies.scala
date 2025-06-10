22209c6ac42b59f9f6c755b087f951b6a424f734
Scala Steward <scala_steward@virtuslab.com>
2025-06-10 01:53:07 +0000

    Update commons-codec to 1.18.0 in 0.11.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val coursier             = "2.1.9"
     val protocGen            = "0.9.9"
     val protobufRuntimeScala = "0.8.16"
-    val commonsCodec         = "1.16.1"
+    val commonsCodec         = "1.18.0"
 
     // For testing
     val annotationApi           = "1.3.2"
