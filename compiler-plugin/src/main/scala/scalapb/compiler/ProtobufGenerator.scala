7217580fcdb7c7f0d64f820d5eaa47d213ed55be
Mark Eibes <mark.eibes@gmail.com>
2025-08-22 16:33:50 +0200

    resolve oneof field name conflicts with imported packages (#1947)
    Include oneof field names in conflict detection to properly generate
    _root_ prefixes when oneof fields conflict with imported package names.
    
    This fixes issue #1911 where ScalaPB would generate incorrect import
    paths for oneof fields that conflict with imported package names.
    
    The fix includes:
    - Enhanced conflict detection in DescriptorImplicits.scala to include oneof field names
    - Updated ProtobufGenerator.scala to properly handle conflicts when generating imports
    - Added comprehensive tests to verify the fix works correctly
    - Fixed test file to use correct generated class name (OneofImportConflictTest)
    
    Closes #1911

--- compiler-plugin/src/main/scala/scalapb/compiler/ProtobufGenerator.scala
+++ compiler-plugin/src/main/scala/scalapb/compiler/ProtobufGenerator.scala
@@ -188,11 +188,15 @@ class ProtobufGenerator(
             .mkString("_root_.com.google.protobuf.ByteString.copyFrom(Array[Byte](", ", ", "))")
       case FieldDescriptor.JavaType.STRING => escapeScalaString(defaultValue.asInstanceOf[String])
       case FieldDescriptor.JavaType.MESSAGE =>
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
         field.getMessageType.scalaType
-          .fullNameWithMaybeRoot(field.getContainingType) + ".defaultInstance"
+          .fullNameWithMaybeRoot(contextNames) + ".defaultInstance"
       case FieldDescriptor.JavaType.ENUM =>
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
         field.getEnumType.scalaType
-          .fullNameWithMaybeRoot(field.getContainingType) + "." + defaultValue
+          .fullNameWithMaybeRoot(contextNames) + "." + defaultValue
           .asInstanceOf[EnumValueDescriptor]
           .scalaName
           .asSymbol
@@ -217,13 +221,22 @@ class ProtobufGenerator(
       case FieldDescriptor.JavaType.BYTE_STRING => Identity
       case FieldDescriptor.JavaType.STRING      => Identity
       case FieldDescriptor.JavaType.MESSAGE =>
-        FunctionApplication(field.getMessageType.scalaType.fullName + ".fromJavaProto")
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
+        FunctionApplication(
+          field.getMessageType.scalaType.fullNameWithMaybeRoot(contextNames) + ".fromJavaProto"
+        )
       case FieldDescriptor.JavaType.ENUM =>
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
         if (!field.legacyEnumFieldTreatedAsClosed())
           MethodApplication("intValue") andThen FunctionApplication(
-            field.getEnumType.scalaType.fullName + ".fromValue"
+            field.getEnumType.scalaType.fullNameWithMaybeRoot(contextNames) + ".fromValue"
           )
-        else FunctionApplication(field.getEnumType.scalaType.fullName + ".fromJavaValue")
+        else
+          FunctionApplication(
+            field.getEnumType.scalaType.fullNameWithMaybeRoot(contextNames) + ".fromJavaValue"
+          )
     }
     baseValueConversion andThen toCustomTypeExpr(field)
   }
@@ -289,12 +302,20 @@ class ProtobufGenerator(
       case FieldDescriptor.JavaType.BYTE_STRING => Identity
       case FieldDescriptor.JavaType.STRING      => Identity
       case FieldDescriptor.JavaType.MESSAGE =>
-        FunctionApplication(field.getMessageType.scalaType.fullName + ".toJavaProto")
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
+        FunctionApplication(
+          field.getMessageType.scalaType.fullNameWithMaybeRoot(contextNames) + ".toJavaProto"
+        )
       case FieldDescriptor.JavaType.ENUM =>
+        val contextNames = field.getContainingType.fields.map(_.scalaName) ++
+          field.getContainingType.getRealOneofs.asScala.map(_.scalaName.nameSymbol)
         if (!field.legacyEnumFieldTreatedAsClosed())
           (MethodApplication("value") andThen maybeBox("_root_.scala.Int.box"))
         else
-          FunctionApplication(field.getEnumType.scalaType.fullName + ".toJavaValue")
+          FunctionApplication(
+            field.getEnumType.scalaType.fullNameWithMaybeRoot(contextNames) + ".toJavaValue"
+          )
     }
   }
 
