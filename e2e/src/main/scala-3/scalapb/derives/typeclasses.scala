e70e0c2d0c71b8a2125e55082a0c14ffe746585e
Scala Steward <scala_steward@virtuslab.com>
2026-02-12 22:35:24 +0000

    Reformat with scalafmt 3.10.7
    Executed command: scalafmt --non-interactive

--- e2e/src/main/scala-3/scalapb/derives/typeclasses.scala
+++ e2e/src/main/scala-3/scalapb/derives/typeclasses.scala
@@ -5,17 +5,17 @@ import scala.compiletime.erasedValue
 import scala.annotation.targetName
 
 trait Show[T]:
-    def show(t: T): String
+  def show(t: T): String
 
 object Show:
-    @targetName("derivedMessage") def derived[T <: GeneratedMessage]: Show[T] = new Show[T]:
-        def show(t: T): String = t.toProtoString
+  @targetName("derivedMessage") def derived[T <: GeneratedMessage]: Show[T] = new Show[T]:
+    def show(t: T): String = t.toProtoString
 
-    @targetName("derivedSealedOneof") def derived[T <: GeneratedSealedOneof]: Show[T] = new Show[T]:
-        def show(t: T): String = "Sealed!"
+  @targetName("derivedSealedOneof") def derived[T <: GeneratedSealedOneof]: Show[T] = new Show[T]:
+    def show(t: T): String = "Sealed!"
 
 trait TC[T]:
-    def tc(t: T): Unit
+  def tc(t: T): Unit
 
 object TC:
-    def derived[T <: GeneratedMessage]: TC[T] = null
+  def derived[T <: GeneratedMessage]: TC[T] = null
