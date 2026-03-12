322c071268a4a0cba35ddd07aa3c69f7a407d403
Scala Steward <scala_steward@virtuslab.com>
2026-03-12 20:30:05 +0000

    Update mockito-core to 5.23.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     // For testing
     val annotationApi           = "1.3.2"
     val cats                    = "2.6.1"
-    val mockito                 = "5.22.0"
+    val mockito                 = "5.23.0"
     val munit                   = "1.2.0"
     val scalaTest               = "3.2.19"
     val scalaTestPlusMockito    = "3.1.0.0"
