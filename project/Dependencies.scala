9c8e5962b6b9b04fa3e59ec95162ec9c15360db8
Scala Steward <scala_steward@virtuslab.com>
2026-03-27 00:55:00 +0000

    Update scalatest to 3.2.20 in 0.11.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val cats                    = "2.6.1"
     val mockito                 = "5.23.0"
     val munit                   = "1.2.0"
-    val scalaTest               = "3.2.19"
+    val scalaTest               = "3.2.20"
     val scalaTestPlusMockito    = "3.1.0.0"
     val scalaTestPlusScalaCheck = "3.2.18.0"
     val utest                   = "0.7.10"
