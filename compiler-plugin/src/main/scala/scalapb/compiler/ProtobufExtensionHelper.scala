fefcad575a7d41544c95c70f49929dffebeb79b6
PJ Fanning <pjfanning@users.noreply.github.com>
2026-05-24 16:28:46 +0100

    scalafmt

--- compiler-plugin/src/main/scala/scalapb/compiler/ProtobufExtensionHelper.scala
+++ compiler-plugin/src/main/scala/scalapb/compiler/ProtobufExtensionHelper.scala
@@ -4,9 +4,9 @@ import com.google.protobuf.GeneratedMessage
 
 /** Works around ambiguous getExtension overload introduced in protobuf 4.35.0.
   *
-  * Both ExtendableMessage (class) and ExtendableMessageOrBuilder (interface) define
-  * getExtension, causing ambiguity in Scala when the concrete descriptor options type is used.
-  * By typing the receiver as ExtendableMessageOrBuilder, only the interface overload is visible.
+  * Both ExtendableMessage (class) and ExtendableMessageOrBuilder (interface) define getExtension,
+  * causing ambiguity in Scala when the concrete descriptor options type is used. By typing the
+  * receiver as ExtendableMessageOrBuilder, only the interface overload is visible.
   */
 private[compiler] object ProtobufExtensionHelper {
   def getExtension[ContainerT <: GeneratedMessage.ExtendableMessage[ContainerT], T](
