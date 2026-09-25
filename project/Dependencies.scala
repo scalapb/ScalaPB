2972b8d7a3b30d92394a00aef05e5b3c69cb262b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-25 02:18:09 +0200

    Update mockito-core to 5.24.0 in 0.11.x (#2224)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     // For testing
     val annotationApi           = "1.3.2"
     val cats                    = "2.6.1"
-    val mockito                 = "5.23.0"
+    val mockito                 = "5.24.0"
     val munit                   = "1.2.0"
     val scalaTest               = "3.2.20"
     val scalaTestPlusMockito    = "3.1.0.0"
